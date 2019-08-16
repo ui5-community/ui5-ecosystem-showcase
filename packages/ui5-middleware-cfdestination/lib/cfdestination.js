425e9dd1803ef866fd2d9a2f198ab960e5fe6995
Volker Buzek <volker.buzek@js-soft.com>
2019-08-16 17:36:02 +0200

    feat: boot @sap/app-router as custom middleware
    - make ui5-app aware of ui5-middleware-cfdestination,
      incl dedicated ui5.yaml config options
    - example xs-app.json
    - start approuter as "shadow" service on dedicated port

---
+++ packages/ui5-middleware-cfdestination/lib/cfdestination.js
@@ -0,0 +1,30 @@
+const approuter = require('@sap/approuter')();
+const fs = require('fs');
+const proxy = require('express-http-proxy');
+
+/**
+ * Custom UI5 Server middleware example
+ *
+ * @param {Object} parameters Parameters
+ * @param {Object} parameters.resources Resource collections
+ * @param {module:@ui5/fs.AbstractReader} parameters.resources.all Reader or Collection to read resources of the
+ *                                        root project and its dependencies
+ * @param {module:@ui5/fs.AbstractReader} parameters.resources.rootProject Reader or Collection to read resources of
+ *                                        the project the server is started in
+ * @param {module:@ui5/fs.AbstractReader} parameters.resources.dependencies Reader or Collection to read resources of
+ *                                        the projects dependencies
+ * @param {Object} parameters.options Options
+ * @param {string} [parameters.options.configuration] Custom server middleware configuration if given in ui5.yaml
+ * @returns {function} Middleware function to use
+ */
+module.exports = function ({resources, options}) {
+    const xsappConfig = JSON.parse(fs.readFileSync(options.configuration.xsappJson, 'utf8'));
+    process.env.destinations = JSON.stringify(options.configuration.destinations);
+    approuter.start({
+        port: options.configuration.port,
+        xsappConfig: xsappConfig
+    });
+    return function (req, res, next) {
+        next();
+    };
+};
