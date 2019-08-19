2a45aec7acc2ed91856f0139e82e65fbf656f408
Volker Buzek <volker.buzek@js-soft.com>
2019-08-19 16:23:05 +0200

    feat: routing, OData v4 binding, example /proxy
    - routing between 2 views
    - introducing a 2nd view that showcases OData v4 via
      ui5-middleware-cfdestination
    - utilizing generic /proxy for retrieving latest UI5 version
    - example implementation of async/await + Promise chain

--- packages/ui5-app/webapp/controller/Main.controller.js
+++ packages/ui5-app/webapp/controller/Main.controller.js
@@ -1,16 +1,33 @@
 sap.ui.define([
-	"test/Sample/controller/BaseController"
+    "test/Sample/controller/BaseController"
 ], function (Controller) {
-	"use strict";
+    "use strict";
 
-	return Controller.extend("test.Sample.controller.Main", {
-		onInit() {
-			let text = "Hello World";
-			console.error(`Peter says: ${text} `);
-		},
+    return Controller.extend("test.Sample.controller.Main", {
+        async onInit() {
+            // let response;
+            // let oLatestUI5 = {
+            //     version: "n/a"
+            // };
+            // try {
+            //     response = await fetch('/proxy/api/v1/latest?format=json');
+            //     oLatestUI5 = await response.json();
+            // } catch (err) {
+            //     console.error(err)
+            // }
+            // this.getModel('LatestUI5').setProperty("/latest", latestU5version.version)
 
-		navFwd(oEvent) {
-			this.getOwnerComponent().getRouter().navTo("RouteOther");
-		}
-	});
+            fetch("/proxy/api/v1/latest?format=json")
+                .then(response => response.json())
+                .then(latestU5version => {
+                    this.getModel('LatestUI5').setProperty("/latest", latestU5version.version)
+                })
+                .catch(err => console.error(err))
+
+        },
+
+        navFwd(oEvent) {
+            this.getOwnerComponent().getRouter().navTo("RouteOther");
+        }
+    });
 });
