ff3bcfd81c7c68affe59c8f8ee26f898cc8de05d
Volker Buzek <volker.buzek@js-soft.com>
2019-08-21 14:58:25 +0200

    feat(err-handling): provide more fine-tuned error handling

--- packages/ui5-middleware-livetranspile/lib/livetranspile.js
+++ packages/ui5-middleware-livetranspile/lib/livetranspile.js
@@ -2,6 +2,10 @@ const babel = require('@babel/core');
 const parseurl = require('parseurl');
 const log = require("@ui5/logger").getLogger("middleware:customtask:livetranspile");
 
+let fileNotFoundError = new Error('file not found!');
+fileNotFoundError.code = 404;
+fileNotFoundError.file = '';
+
 /**
  * Custom UI5 Server middleware example
  *
@@ -20,16 +24,21 @@ const log = require("@ui5/logger").getLogger("middleware:customtask:livetranspil
 module.exports = function ({resources, options}) {
     return (req, res, next) => {
         if (req.path.endsWith('.js')) {
-            options.configuration.debug ? log.info(`transpiling ${req.path}...`) : null;
+            options.configuration.debug ? log.info(`handling ${req.path}...`) : null;
             const pathname = parseurl(req).pathname;
 
             // grab the file via @ui5/fs.AbstractReader API
             return resources.rootProject.byPath(pathname)
                 .then(resource => {
+                    if (!resource) {
+                        fileNotFoundError.file = pathname;
+                        throw fileNotFoundError;
+                    }
                     // read file into string
                     return resource.getString();
                 })
                 .then(source => {
+                    options.configuration.debug ? log.info(`...transpiling!`) : null;
                     return babel.transformAsync(source, {
                             filename: pathname, // necessary for source map <-> source assoc
                             sourceMaps: 'inline',
@@ -49,7 +58,11 @@ module.exports = function ({resources, options}) {
                     res.end(result.code);
                 })
                 .catch(err => {
-                    log.warn(err);
+                    if (err.code === 404) {
+                        log.warn(`...file not found: ${err.file}!`);
+                    } else {
+                        log.error(err);
+                    }
                     next();
                 })
         } else {
