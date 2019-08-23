3a93266c9ef1ac555b54a8878f31acd6cdb68ef7
Volker Buzek <volker.buzek@js-soft.com>
2019-08-23 09:33:45 +0200

    refactor: use @ui/fs.AbstractReader API (#14)
    * refactor: use @ui/fs.AbstractReader API
    
    * feat(err-handling): provide more fine-tuned error handling

--- packages/ui5-middleware-livetranspile/lib/livetranspile.js
+++ packages/ui5-middleware-livetranspile/lib/livetranspile.js
@@ -1,6 +1,10 @@
 const babel = require('@babel/core');
-const path = require('path');
+const parseurl = require('parseurl');
 const log = require("@ui5/logger").getLogger("middleware:customtask:livetranspile");
+
+let fileNotFoundError = new Error('file not found!');
+fileNotFoundError.code = 404;
+fileNotFoundError.file = '';
 
 /**
  * Custom UI5 Server middleware example
@@ -20,24 +24,45 @@ const log = require("@ui5/logger").getLogger("middleware:customtask:livetranspil
 module.exports = function ({resources, options}) {
     return (req, res, next) => {
         if (req.path.endsWith('.js')) {
-            options.configuration.debug ? log.info(`transpiling ${req.path}...`) : null;
-            babel.transformFileAsync(path.join(process.cwd(), 'webapp', req.path), {
-                sourceMaps: "both",
-                presets: [
-                    ["@babel/preset-env", {
-                        "targets": {
-                            "browsers": "last 2 versions, ie 10-11"
-                        }
-                    }]
-                ]
-            })
+            options.configuration.debug ? log.info(`handling ${req.path}...`) : null;
+            const pathname = parseurl(req).pathname;
 
+            // grab the file via @ui5/fs.AbstractReader API
+            return resources.rootProject.byPath(pathname)
+                .then(resource => {
+                    if (!resource) {
+                        fileNotFoundError.file = pathname;
+                        throw fileNotFoundError;
+                    }
+                    // read file into string
+                    return resource.getString();
+                })
+                .then(source => {
+                    options.configuration.debug ? log.info(`...transpiling!`) : null;
+                    return babel.transformAsync(source, {
+                            filename: pathname, // necessary for source map <-> source assoc
+                            sourceMaps: 'inline',
+                            presets: [
+                                ["@babel/preset-env", {
+                                    "targets": {
+                                        "browsers": "last 2 versions, ie 10-11"
+                                    }
+                                }]
+                            ]
+                        }
+                    )
+                })
                 .then(result => {
+                    // send out transpiled source + source map
                     res.type('.js');
-                    res.send(result.code);
+                    res.end(result.code);
                 })
                 .catch(err => {
-                    log.warn(err);
+                    if (err.code === 404) {
+                        log.warn(`...file not found: ${err.file}!`);
+                    } else {
+                        log.error(err);
+                    }
                     next();
                 })
         } else {
