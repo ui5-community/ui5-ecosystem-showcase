ac334683efa300dc703754d8140dcf50b1527427
Volker Buzek <volker.buzek@js-soft.com>
2019-10-25 14:56:41 +0200

    refactor: only display errors in karma console

--- packages/ui5-app/karma.conf.js
+++ packages/ui5-app/karma.conf.js
@@ -2,7 +2,9 @@ module.exports = function (config) {
     config.set({
 
         frameworks: ["ui5"],
-        browsers: ["Chrome"]
-
+        browsers: ["Chrome"],
+        browserConsoleLogOptions: {
+			level: "error"
+		}
     });
 };
