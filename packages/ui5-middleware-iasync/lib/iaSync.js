f31c540601882d6aee84ca579140fc079dffd5e2
Volker Buzek <volker.buzek@js-soft.com>
2019-11-25 16:06:58 +0100

    wip: add iasync middleware

---
+++ packages/ui5-middleware-iasync/lib/iaSync.js
@@ -0,0 +1,34 @@
+const browserSync = require("browser-sync");
+const log = require("@ui5/logger").getLogger("server:custommiddleware:iasync");
+
+/**
+ * Custom UI5 Server middleware example
+ *
+ * @param {Object} parameters Parameters
+ * @param {Object} parameters.resources Resource collections
+ * @param {module:@ui5/fs.AbstractReader} parameters.resources.all Reader or Collection to read resources of the
+ *                                        root project and its dependencies
+ * @param {module:@ui5/fs.AbstractReader} parameters.resources.rootProject Reader or Collection to read resources of
+ *                                        the project the server is started in
+ * @param {module:@ui5/fs.AbstractReader} parameters.resources.dependencies Reader or Collection to read resources of
+ *                                        the projects dependencies
+ * @param {Object} parameters.options Options
+ * @param {string} [parameters.options.configuration] Custom server middleware configuration if given in ui5.yaml
+ * @returns {function} Middleware function to use
+ */
+module.exports = function({ resources, options }) {
+    const bs = browserSync.create().init(
+        {
+            socket: {
+                domain: "localhost:3000"
+            },
+            host: "localhost:3000",
+            logSnippet: false,
+            notify: false
+        },
+        (err, instance) => {
+            log.info(`started`)
+        }
+    );
+    return require("connect-browser-sync")(bs);
+};
