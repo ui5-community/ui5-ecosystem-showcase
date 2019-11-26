4132199f2f7ca5090e09dfda1c2244b14bf04034
Volker Buzek <volker.buzek@js-soft.com>
2019-11-26 10:16:38 +0100

    refactor: better naming

--- packages/ui5-middleware-iasync/lib/inject.js
+++ packages/ui5-middleware-iasync/lib/inject.js
@@ -2,27 +2,29 @@ const fs = require("fs");
 const injector = require("connect-injector");
 const path = require("path");
 
+// read in UI5 "enhancements" that enable syncing click actions
+// browsersync only syncs "clicks", but e.g. no "taps"
 const customUI5Html = fs.readFileSync(path.join(`${__dirname}`, "ui5mangler.html"), { encoding: "utf-8" });
 
 // mostly directly taken from https://github.com/schmich/connect-browser-sync/blob/master/index.js
 module.exports = function injectBrowserSyncAndCustomUI5(browserSync, options = {}) {
     let snippet = "";
-    let tag = customUI5Html + "</body>";
+    let injectedHtml = customUI5Html + "</body>";
     let find = /<\/body>(?!(.|\n)*<\/body>)/gi;
 
-    browserSync.emitter.on("service:running", function(data) {
+    browserSync.emitter.on("service:running", (data) => {
         if (!snippet) {
             snippet = data.options.get("snippet");
         }
     });
     return injector(
-        function(req, res) {
+        (req, res) => {
             let contentType = res.getHeader("content-type");
             return contentType && contentType.toLowerCase().indexOf("text/html") >= 0;
         },
         function converter(content, req, res, callback) {
             function inject() {
-                let injected = content.toString().replace(find, snippet + tag);
+                let injected = content.toString().replace(find, snippet + injectedHtml);
                 callback(null, injected);
             }
             if (!snippet) {
