696a4165724765f192afe129913d7d779c37071b
Volker Buzek <volker.buzek@js-soft.com>
2019-11-26 09:32:24 +0100

    wip: middleware + inject
    - move browersync middleware inline
    - add custom UI5 html to injector

--- packages/ui5-middleware-iasync/lib/iaSync.js
+++ packages/ui5-middleware-iasync/lib/iaSync.js
@@ -1,6 +1,78 @@
 const browserSync = require("browser-sync");
 const log = require("@ui5/logger").getLogger("server:custommiddleware:iasync");
 
+const injector = require("connect-injector");
+
+const customUI5Html = `
+<script>
+        sap.ui.require([
+            "sap/m/Button",
+            "sap/ui/unified/calendar/Month",
+            "sap/ui/unified/calendar/YearPicker"
+        ], (Button, Month, YearPicker) => {
+
+            Button.prototype.onclick = function () {
+                Button.prototype.ontap.apply(this, arguments);
+            }
+
+            Month.prototype.onclick = function () {
+                Month.prototype.onmousedown.apply(this, arguments);
+                Month.prototype.onkeydown.apply(this, arguments);
+                Month.prototype.onsapselect.apply(this, arguments);
+            }
+            YearPicker.prototype.onclick = function () {
+                YearPicker.prototype.onmousedown.apply(this, arguments);
+                YearPicker.prototype.onsapselect.apply(this, arguments);
+            }
+
+        });
+    </script>
+`;
+
+function injectBrowserSync(browserSync, options) {
+    var snippet = "";
+    var options = options || {};
+
+    if (options.injectHead === true) {
+        var tag = customUI5Html + "</head>";
+        var find = /<\/head>/gi;
+    } else {
+        var tag = customUI5Html + "</body>";
+        var find = /<\/body>(?!(.|\n)*<\/body>)/gi;
+    }
+
+    browserSync.emitter.on("service:running", function(data) {
+        if (!snippet) {
+            snippet = data.options.get("snippet");
+        }
+    });
+
+    return injector(
+        function(req, res) {
+            var contentType = res.getHeader("content-type");
+            return contentType && contentType.toLowerCase().indexOf("text/html") >= 0;
+        },
+        function converter(content, req, res, callback) {
+            function inject() {
+                var injected = content.toString().replace(find, snippet + tag);
+                callback(null, injected);
+            }
+
+            if (!snippet) {
+                // We don't have the snippet from BrowserSync yet.
+                // Block the response until we get it.
+                browserSync.emitter.on("service:running", function(data) {
+                    if (!snippet) {
+                        snippet = data.options.get("snippet");
+                    }
+                    inject();
+                });
+            } else {
+                inject();
+            }
+        }
+    );
+}
 /**
  * Custom UI5 Server middleware example
  *
@@ -27,8 +99,12 @@ module.exports = function({ resources, options }) {
             notify: false
         },
         (err, instance) => {
-            log.info(`started`)
+            log.info(`started`);
         }
     );
-    return require("connect-browser-sync")(bs);
+    // return (req, res, next) => {
+    // const ret = require("connect-browser-sync")(bs);
+    const ret = injectBrowserSync(bs);
+    return ret;
+    // }
 };
