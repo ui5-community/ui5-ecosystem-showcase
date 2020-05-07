e729e9ff716c0d305133bbdcd058add04403a101
Maximilian Moehl <maximilian.moehl@sap.com>
2020-05-07 12:08:16 +0200

    feat(pwa): added ui5-task-pwa-enabler as a new package

---
+++ packages/ui5-task-pwa-enabler/templates/offlineCopy.js
@@ -0,0 +1,49 @@
+// This is the "Offline copy of pages" service worker
+
+const CACHE = "offline-copy-{{timestamp}}";
+
+self.addEventListener("install", function (event) {
+  console.log("[Service-Worker] Install Event processing");
+});
+
+// If any fetch fails, it will look for the request in the cache and serve it from there first
+self.addEventListener("fetch", function (event) {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(function (response) {
+        console.log("[Service-Worker] add page to offline cache: " + response.url);
+
+        // If request was success, add or update it in the cache
+        event.waitUntil(updateCache(event.request, response.clone()));
+
+        return response;
+      })
+      .catch(function (error) {
+        console.log("[Service-Worker] Network request Failed. Serving content from cache: " + error);
+        return fromCache(event.request);
+      })
+  );
+});
+
+function fromCache(request) {
+  // Check to see if you have it in the cache
+  // Return response
+  // If not in the cache, then return error page
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      if (!matching || matching.status === 404) {
+        return Promise.reject("no-match");
+      }
+
+      return matching;
+    });
+  });
+}
+
+function updateCache(request, response) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.put(request, response);
+  });
+}
