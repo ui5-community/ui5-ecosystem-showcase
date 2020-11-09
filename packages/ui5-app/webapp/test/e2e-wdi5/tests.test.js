1d12a03071a6d60cbdba036f5b77eea0e91d11b5
Volker Buzek <volker.buzek@js-soft.com>
2020-11-09 22:25:13 +0100

    refactor: wdi5 tests

--- packages/ui5-app/webapp/test/e2e-wdi5/tests.test.js
+++ packages/ui5-app/webapp/test/e2e-wdi5/tests.test.js
@@ -1,53 +1,54 @@
 const title = {
+    wdio_ui5_key: 'MainViewTitle',
+    forceSelect: true,
     selector: {
         viewName: "test.Sample.view.Main",
         controlType: "sap.m.Title",
         properties: {
-            text: "#UI5 demo",
-        },
-    },
-};
+            text: "#UI5 demo"
+        }
+    }
+}
 
 const navFwdButton = {
     selector: {
         viewName: "test.Sample.view.Main",
-        id: "NavButton",
-    },
-};
+        id: "NavButton"
+    }
+}
 
 const list = {
     selector: {
         viewName: "test.Sample.view.Other",
-        id: "PeopleList",
-    },
-};
+        id: "PeopleList"
+    }
+}
 
 const backButton = {
     selector: {
-        // viewName: "test.Sample.view.Other",
-        id: "__page0-navButton",
-    },
-};
+        id: "__page0-navButton"
+    }
+}
 
 describe("navigation", () => {
     it("should see the initial page of the app", () => {
-        expect(browser.asControl(title).getText()).toBe("#UI5 demo");
-    });
+        expect(browser.asControl(title).getText()).toBe("#UI5 demo")
+    })
 
     it("should navigate to the list page and back", () => {
-        browser.asControl(navFwdButton).firePress();
-        expect(browser.asControl(list).getVisible()).toBeTruthy();
+        browser.asControl(navFwdButton).firePress()
+        expect(browser.asControl(list).getVisible()).toBeTruthy()
 
-        browser.asControl(backButton).firePress();
+        browser.asControl(backButton).firePress()
 
-        expect(browser.asControl(title).getVisible()).toBeTruthy();
-    });
-});
+        expect(browser.asControl(title).getVisible()).toBeTruthy()
+    })
+})
 
 describe("binding", () => {
     it("Other view: PeopleList: items aggregation + amount of items", () => {
-        browser.asControl(navFwdButton).firePress();
+        browser.asControl(navFwdButton).firePress()
         const items = browser.asControl(list).getAggregation("items")
-        expect(items.length).toBeGreaterThanOrEqual(1);
-    });
-});
+        expect(items.length).toBeGreaterThanOrEqual(1)
+    })
+})
