97dd3ce75d3be514033e0268845e02bc02692075
Volker Buzek <volker.buzek@js-soft.com>
2020-11-09 16:28:13 +0100

    test(binding,opa5): aggregation binding tests
    w/ the downside of a non-existing
    AggregationLengthGreaterOrEquals
    matcher :(

--- packages/ui5-app/webapp/test/integration/AllJourneys.js
+++ packages/ui5-app/webapp/test/integration/AllJourneys.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	"sap/ui/test/Opa5",
 	"./arrangements/Startup",
-	"./NavigationJourney"
+	"./NavigationJourney",
+	"./BindingJourney"
 ], function (Opa5, Startup) {
 	"use strict";
 
--- packages/ui5-app/webapp/test/integration/pages/Other.js
+++ packages/ui5-app/webapp/test/integration/pages/Other.js
@@ -1,54 +1,74 @@
-sap.ui.define(["sap/ui/test/Opa5", "sap/ui/test/matchers/AggregationLengthEquals", "sap/ui/test/actions/Press"], function (
-    Opa5,
-	AggregationLengthEquals,
-	Press
-) {
-    "use strict";
-    var sViewName = "Other";
+sap.ui.define(
+    [
+        "sap/ui/test/Opa5",
+        "sap/ui/test/matchers/AggregationLengthEquals",
+        "sap/ui/test/matchers/AggregationFilled",
+        "sap/ui/test/actions/Press",
+    ],
+    function (Opa5, AggregationLengthEquals, AggregationFilled, Press) {
+        "use strict";
+        var sViewName = "Other";
 
-    Opa5.createPageObjects({
-        onTheOtherView: {
-            actions: {
-				iNavigateBack: function() {
-					return this.waitFor({
-						viewName: sViewName,
-						id: /-navButton/,
-						actions: new Press(),
-						success: function() {
-							Opa5.assert.ok(true, "nav back via sap.m.Page's nav back button!")
-						},
-						errorMessage: "couldn't locate or use the sap.m.Page's nav back button"
-					})
-				}
-			},
-
-            assertions: {
-                iShouldSeeTheList: function () {
-                    return this.waitFor({
-                        viewName: sViewName,
-                        success: function () {
-                            Opa5.assert.ok(true, "The Other view is displayed");
-                        },
-                        errorMessage: "Did not find the Other view",
-                    });
+        Opa5.createPageObjects({
+            onTheOtherView: {
+                actions: {
+                    iNavigateBack: function () {
+                        return this.waitFor({
+                            viewName: sViewName,
+                            id: /-navButton/,
+                            actions: new Press(),
+                            success: function () {
+                                Opa5.assert.ok(true, "nav back via sap.m.Page's nav back button!");
+                            },
+                            errorMessage: "couldn't locate or use the sap.m.Page's nav back button",
+                        });
+                    },
                 },
 
-                theListContainsAtLeastItems: function (iNumberOfItems) {
-                    return this.waitFor({
-                        viewName: sViewName,
-                        id: "PeopleList",
-                        matchers: [
-                            new AggregationLengthEquals({
-                                name: "items",
-                                length: 20,
-                            }),
-                        ],
-                        success: function () {
-                            Opa5.assert.ok(true, "list contains at least " + iNumberOfItems + " items");
-                        },
-                    });
+                assertions: {
+                    iShouldSeeTheList: function () {
+                        return this.waitFor({
+                            viewName: sViewName,
+                            success: function () {
+                                Opa5.assert.ok(true, "The Other view is displayed");
+                            },
+                            errorMessage: "Did not find the Other view",
+                        });
+                    },
+
+                    theListShouldBeBound: function () {
+                        return this.waitFor({
+                            viewName: sViewName,
+                            id: "PeopleList",
+                            matchers: [
+                                new AggregationFilled({
+                                    name: "items",
+                                }),
+                            ],
+                            success: function () {
+                                Opa5.assert.ok(true, "list is bound");
+                            },
+                            errorMessage: "list is not bound!",
+                        });
+                    },
+
+                    theListContainsAtLeastItems: function (iNumberOfItems) {
+                        return this.waitFor({
+                            viewName: sViewName,
+                            id: "PeopleList",
+                            matchers: [
+                                new AggregationLengthEquals({
+                                    name: "items",
+                                    length: 20, // SHOULD be iNumberOfItems,
+                                }),
+                            ],
+                            success: function () {
+                                Opa5.assert.ok(true, "list contains at least " + iNumberOfItems + " items");
+                            },
+                        });
+                    },
                 },
             },
-        },
-    });
-});
+        });
+    }
+);
