66a7cf776395a3a5869762411e3814d671a69f95
Volker Buzek <volker.buzek@js-soft.com>
2020-11-09 16:16:55 +0100

    test(navigation): let opa5 nav the app
    + refactor "navBack": flaky history API?

--- packages/ui5-app/webapp/test/integration/pages/Main.js
+++ packages/ui5-app/webapp/test/integration/pages/Main.js
@@ -1,13 +1,22 @@
 sap.ui.define([
-	"sap/ui/test/Opa5"
-], function (Opa5) {
+	"sap/ui/test/Opa5",
+	"sap/ui/test/actions/Press"
+], function (Opa5, Press) {
 	"use strict";
 	var sViewName = "Main";
 
 	Opa5.createPageObjects({
 		onTheAppPage: {
 
-			actions: {},
+			actions: {
+				iPressTheNavButton: function () {
+					return this.waitFor({
+						 viewName: sViewName,
+						 id: "NavButton",
+						 actions: new Press()
+					})
+				},
+			},
 
 			assertions: {
 
@@ -19,6 +28,16 @@ sap.ui.define([
 						},
 						errorMessage: "Did not find the Main view"
 					});
+				},
+				iShouldSeeTheNavButton: function () {
+					return this.waitFor({
+						 viewName: sViewName,
+						 id: "NavButton",
+						 success: function () {
+							Opa5.assert.ok(true, "The Main view contains the nav button!");
+						},
+						errorMessage: "Did not find the nav button on the Main view!"
+					})
 				}
 			}
 		}
---
+++ packages/ui5-app/webapp/test/integration/pages/Other.js
@@ -0,0 +1,54 @@
+sap.ui.define(["sap/ui/test/Opa5", "sap/ui/test/matchers/AggregationLengthEquals", "sap/ui/test/actions/Press"], function (
+    Opa5,
+	AggregationLengthEquals,
+	Press
+) {
+    "use strict";
+    var sViewName = "Other";
+
+    Opa5.createPageObjects({
+        onTheOtherView: {
+            actions: {
+				iNavigateBack: function() {
+					return this.waitFor({
+						viewName: sViewName,
+						id: /-navButton/,
+						actions: new Press(),
+						success: function() {
+							Opa5.assert.ok(true, "nav back via sap.m.Page's nav back button!")
+						},
+						errorMessage: "couldn't locate or use the sap.m.Page's nav back button"
+					})
+				}
+			},
+
+            assertions: {
+                iShouldSeeTheList: function () {
+                    return this.waitFor({
+                        viewName: sViewName,
+                        success: function () {
+                            Opa5.assert.ok(true, "The Other view is displayed");
+                        },
+                        errorMessage: "Did not find the Other view",
+                    });
+                },
+
+                theListContainsAtLeastItems: function (iNumberOfItems) {
+                    return this.waitFor({
+                        viewName: sViewName,
+                        id: "PeopleList",
+                        matchers: [
+                            new AggregationLengthEquals({
+                                name: "items",
+                                length: 20,
+                            }),
+                        ],
+                        success: function () {
+                            Opa5.assert.ok(true, "list contains at least " + iNumberOfItems + " items");
+                        },
+                    });
+                },
+            },
+        },
+    });
+});
