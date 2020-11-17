70fab6ee93e7935f958a95d3da077e12157eec47
Volker Buzek <volker.buzek@js-soft.com>
2020-11-17 17:59:53 +0100

    test(uiveri5): page objects best practice demo

--- packages/ui5-app/webapp/test/e2e/navigation.spec.js
+++ packages/ui5-app/webapp/test/e2e/navigation.spec.js
@@ -1,33 +1,17 @@
+const mainPageObject = require("./pages/main.view");
+const otherPageObject = require("./pages/other.view");
+
 describe("navigation", function () {
     it("should see the initial page of the app", function () {
-        const title = element(
-            by.control({
-                viewName: "test.Sample.view.Main",
-                controlType: "sap.m.Title",
-                properties: {
-                    text: "#UI5 demo",
-                },
-            })
-        );
-        // don't know why this isn't title.asControl() ... :(
-        expect(title.getText()).toBe("#UI5 demo");
+        Given.iStartMyApp();
+        Then.onTheMainPage.iShouldSeeTheApp();
     });
 
-    it("should navigate to the list page and back", function() {
-        element(
-            by.control({
-                viewName: "test.Sample.view.Main",
-                id: "NavButton"
-            })
-        ).click();
+    it("should navigate to the list page and back", function () {
+        When.onTheMainPage.iPressTheNavButton();
+        Then.onTheOtherPage.iShouldSeeTheList();
 
-        const list = element(
-            by.control({
-                viewName: "test.Sample.view.Other",
-                id: "PeopleList"
-            })
-        )
-
-        expect(list.asControl().getProperty("visible")).toBe(true)
-    })
+        When.onTheOtherPage.iNavigateBack();
+        Then.onTheMainPage.iShouldSeeTheNavButton();
+    });
 });
