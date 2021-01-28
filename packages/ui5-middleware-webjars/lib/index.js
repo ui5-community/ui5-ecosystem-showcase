b9c7b0591dedc58a92786d7e537d4cf752665ec7
Peter Muessig <petermuessig@users.noreply.github.com>
2021-01-29 00:13:27 +0100

    feat(webjars): enable support for classpath file (#471)

--- packages/ui5-middleware-webjars/lib/index.js
+++ packages/ui5-middleware-webjars/lib/index.js
@@ -1,6 +1,7 @@
 const log = require("@ui5/logger").getLogger("server:custommiddleware:webjars")
 
 const path = require("path");
+const { existsSync } = require("fs");
 const { readdir: readdirAsync, readFile: readFileAsync } = require("fs").promises;
 
 const JSZip = require("jszip");
@@ -30,45 +31,86 @@ const JSZip = require("jszip");
 module.exports = async ({ resources, options, middlewareUtil }) => {
 
     const isDebug = options && options.configuration && options.configuration.debug;
-    const rootPath = options && options.configuration && options.configuration.rootPath || "jars";
+    const rootPath = options && options.configuration && options.configuration.rootPath;
     const jarRootPath = options && options.configuration && options.configuration.jarRootPath || "META-INF/resources/";
+    const classpathFile = options && options.configuration && options.configuration.classpathFile;
 
-    const basePath = path.join(process.cwd(), rootPath);
-    if (isDebug) {
-        log.info(`Scanning directory ${basePath} for JAR files...`);
+    // read classpath file or scan the JAR directory
+    let files;
+    if (classpathFile) {
+        // read the classpath file which contains the JAR files separated by the
+        // path.delimiter e.g. created via the maven-dependency-plugin by executing:
+        //   > mvn clean dependency:build-classpath -Dmdep.cpFile=target/classpath
+        const cpFile = path.join(process.cwd(), classpathFile);
+        if (existsSync(cpFile)) {
+            if (isDebug) {
+                log.info(`Using classpath file ${classpathFile} to determine new JAR files...`);
+            }
+            const classpath = await readFileAsync(cpFile, { encoding: "utf8" });
+            files = classpath.split(path.delimiter);
+        } else {
+            log.error(`The classpath file ${classpathFile} doesn't exist!`);
+            throw new Error(`The classpath file ${classpathFile} doesn't exist!`);
+        }
+    } else if (rootPath) {
+        // scanning the root path for the JAR files
+        const basePath = path.join(process.cwd(), rootPath);
+        if (existsSync(basePath)) {
+            if (isDebug) {
+                log.info(`Scanning directory ${basePath} for JAR files...`);
+            }
+            files = (await readdirAsync(basePath)).map(file => {
+                return path.join(basePath, file);
+            });
+        } else {
+            log.error(`The rootPath ${basePath} of the JAR files doesn't exist!`);
+            throw new Error(`The rootPath ${basePath} of the JAR files doesn't exist!`);
+        }
+    } else {
+        // if no rootPath and no classpathFile has been defined, we disable the middleware
+        log.warn(`Neither rootPath nor classpathFile has been defined!`);
+        return async function noopWebJARs(req, res, next) { next(); };
     }
 
-    // list all JAR files
-    const files = await readdirAsync(basePath);
+    // open and read all JAR files
     const jars = await Promise.all(files.filter(file => {
         return file.endsWith(".jar");
     }).map(async file => {
+        const startTime = Date.now();
         if (isDebug) {
             log.info(`  - Found JAR file ${file}`);
         }
-        const data = await readFileAsync(path.join(basePath, file));
-        return JSZip.loadAsync(data);
+        const data = await readFileAsync(file);
+        const content = await JSZip.loadAsync(data);
+        return {
+            file: file,
+            content: content,
+            startTime: startTime
+        };
     }));
 
-    // read the content of the JAR files into a map
+    // read the resources of the JAR files
     let jarResources = {};
     await Promise.all(jars.map(async jar => {
-        await Promise.all(Object.keys(jar.files).filter(entry => {
+        await Promise.all(Object.keys(jar.content.files).filter(entry => {
             return entry.startsWith(jarRootPath) && !entry.endsWith("/");
         }).map(async entry => {
-            jarResources[entry] = await jar.file(entry).async("string");
+            jarResources[entry] = await jar.content.file(entry).async("string");
         }));
+        if (isDebug) {
+            log.info(`  - Loading JAR file ${jar.file} took ${(Date.now() - jar.startTime)}ms`);
+        }
     }));
 
     return async function serveWebJARs(req, res, next) {
 
         const pathname = middlewareUtil.getPathname(req);
-        const jarPath = jarRootPath + pathname.substr(1);
+        const jarPath = path.join(jarRootPath, pathname.substr(1));
 
         const jarResource = jarResources[jarPath];
         if (jarResource) {
             if (isDebug) {
-                log.info(`Serving ${pathname} from JAR!`);
+                log.info(`Serving ${pathname}`);
             }
 
             // determine charset and content-type
@@ -76,9 +118,7 @@ module.exports = async ({ resources, options, middlewareUtil }) => {
             if (pathname.endsWith(".properties")) {
                 contentType = "text/plain";
             }
-            if (!res.getHeader("Content-Type")) {
-                res.setHeader("Content-Type", contentType);
-            }
+            res.setHeader("Content-Type", contentType);
 
             res.send(jarResource);
             res.end();
