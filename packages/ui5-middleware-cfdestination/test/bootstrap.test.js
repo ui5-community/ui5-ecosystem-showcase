5a847ffaef15af7b5e2e249628687e02d01c7260
Volker Buzek <volker.buzek@js-soft.com>
2021-05-15 13:58:02 +0200

    test: ui5-app via ui5 serve skeleton
    including custom xs-app.json

--- packages/ui5-middleware-cfdestination/test/bootstrap.test.js
+++ packages/ui5-middleware-cfdestination/test/bootstrap.test.js
@@ -1,11 +1,9 @@
 const fs = require("fs-extra")
 const path = require("path")
+const { spawn } = require("child_process")
 const test = require('ava')
 const waitOn = require("wait-on")
 
-const exec = require('child_process').exec
-const normalizer = require("@ui5/project").normalizer
-const server = require("@ui5/server").server
 
 test.before(async () => {
     // bak xs-app.json
@@ -13,7 +11,7 @@ test.before(async () => {
     const dest = path.resolve(__dirname, "../../ui5-app/xs-app.json.bak")
     await fs.move(src, dest, { overwrite: true })
 })
-test.after(async () => {
+test.after.always(async () => {
     // restore bak'ed xs-app.json
     const src = path.resolve(__dirname, "../../ui5-app/xs-app.json.bak")
     const dest = path.resolve(__dirname, "../../ui5-app/xs-app.json")
@@ -23,8 +21,8 @@ test.after(async () => {
     await fs.remove(path.resolve(__dirname, "../../ui5-app/default-env.json"))
 })
 test.beforeEach(async () => {
-    // rm xs-app.json from ui5 sample app
-    // await fs.remove(path.resolve(__dirname, "../../ui5-app/xs-app.json"))
+    // clean route slate
+    await fs.remove(path.resolve(__dirname, "../../ui5-app/xs-app.json"))
 })
 test.afterEach(async () => {
     // rm xs-app.json from ui5 sample app
@@ -32,18 +30,27 @@ test.afterEach(async () => {
 })
 
 test('auth in yaml, xsuaa auth in route', async t => {
+    // prep ui5 server runtime config/env files
     const ui5 = { yaml: path.resolve("./test/auth/ui5-auth-in-yaml.yaml") }
     const xsapp = { json: path.resolve("./test/auth/xs-app.json") }
+    const defaultEnv = { json: path.resolve("./test/auth/default-env.json") }
+
+    // prep authenticated routes + authentication config
     await fs.ensureSymlink(xsapp.json, path.resolve(__dirname, "../../ui5-app/xs-app.json"))
+    await fs.ensureSymlink(defaultEnv.json, path.resolve(__dirname, "../../ui5-app/default-env.json"))
 
-    exec(`yarn workspace ui5 serve --port 1081 --config ${ui5.yaml}`, (error, stdout, stderr) => {})
-    await waitOn({resources:["tcp:1081"]})
-
-    // const docroot = await normalizer.generateProjectTree({ cwd: path.resolve(__dirname, "../../ui5-app") })
-    // const s = await server.serve(docroot, { port: 1081 })
+    // start ui5-app with modified route(s) and config
+    const child = spawn(`yarn workspace ui5-app ui5 serve --port 1081 --config ${ui5.yaml}`, {
+        stdio: 'inherit',
+        shell: true
+    })
+    // wait for ui5 server and app router to boot
+    await waitOn({ resources: ["tcp:1081", "tcp:1091"] }) 
 
 
     t.pass()
+
+    child.kill() // don't take it literally
 
 })
 
