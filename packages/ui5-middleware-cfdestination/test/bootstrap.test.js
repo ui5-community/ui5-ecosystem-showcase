e41d70d6b523be501af54a70d59b35275988024d
Volker Buzek <volker.buzek@js-soft.com>
2021-05-15 15:04:57 +0200

    test(app): validate index and oauth redirect

--- packages/ui5-middleware-cfdestination/test/bootstrap.test.js
+++ packages/ui5-middleware-cfdestination/test/bootstrap.test.js
@@ -1,9 +1,9 @@
 const fs = require("fs-extra")
 const path = require("path")
+const request = require("supertest")
 const { spawn } = require("child_process")
 const test = require('ava')
 const waitOn = require("wait-on")
-
 
 test.before(async () => {
     // bak xs-app.json
@@ -29,7 +29,7 @@ test.afterEach(async () => {
     await fs.remove(path.resolve(__dirname, "../../ui5-app/xs-app.json"))
 })
 
-test('auth in yaml, xsuaa auth in route', async t => {
+test.serial('auth in yaml, xsuaa auth in route', async t => {
     // prep ui5 server runtime config/env files
     const ui5 = { yaml: path.resolve("./test/auth/ui5-auth-in-yaml.yaml") }
     const xsapp = { json: path.resolve("./test/auth/xs-app.json") }
@@ -41,14 +41,20 @@ test('auth in yaml, xsuaa auth in route', async t => {
 
     // start ui5-app with modified route(s) and config
     const child = spawn(`yarn workspace ui5-app ui5 serve --port 1081 --config ${ui5.yaml}`, {
-        stdio: 'inherit',
+        // stdio: 'inherit', // > don't include stdout in test output
         shell: true
     })
     // wait for ui5 server and app router to boot
-    await waitOn({ resources: ["tcp:1081", "tcp:1091"] }) 
+    await waitOn({ resources: ["tcp:1081", "tcp:1091"] })
 
+    const app = request("http://localhost:1081")
+    // test for the app being started correctly
+    const responseIndex = await app.get("/index.html")
+    t.is(responseIndex.status, 200, "http 200 on index")
 
-    t.pass()
+    const responseIdpRedirect = await app.get("/backend/")
+    t.is(responseIdpRedirect.status, 200)
+    t.true(responseIdpRedirect.text.includes("https://authentication.eu10.hana.ondemand.com/oauth/authorize"), "oauth endpoint redirect injected")
 
     child.kill() // don't take it literally
 
