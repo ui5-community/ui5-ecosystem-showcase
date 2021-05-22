6c0def8d31381d0d325fba059b4d13107316d2f7
Volker Buzek <volker.buzek@js-soft.com>
2021-05-22 14:15:40 +0200

    test(ports): randomize app router port per test

--- packages/ui5-middleware-cfdestination/test/boot.test.js
+++ packages/ui5-middleware-cfdestination/test/boot.test.js
@@ -2,6 +2,7 @@ const crypto = require("crypto")
 const fs = require("fs-extra")
 const getPort = require('get-port')
 const path = require("path")
+const replace = require('replace-in-file')
 const request = require("supertest")
 const { spawn } = require("child_process")
 const test = require('ava')
@@ -17,7 +18,7 @@ const waitOn = require("wait-on")
 async function copyUI5app(tmpDir) {
     await fs.mkdir(tmpDir)
     const filterFn = (src, _) => {
-        const yo = ["node_modules", "dist"].find(node => src.endsWith(node))
+        const yo = ["node_modules", "dist", "ui5.yaml"].find(node => src.endsWith(node))
         return yo === undefined ? true : false
     }
     await fs.copy(path.resolve(__dirname, "../../ui5-app"), tmpDir, {
@@ -32,7 +33,10 @@ test.beforeEach(async (t) => {
     await copyUI5app(t.context.tmpDir)
 
     // dynamic port allocation for ui5 serve
-    t.context.port = await getPort()
+    t.context.port = {
+        ui5Sserver: await getPort(),
+        appRouter: await getPort()
+    }
 })
 
 test.afterEach.always(async t => {
@@ -42,8 +46,11 @@ test.afterEach.always(async t => {
 
 
 test('auth in yaml, xsuaa auth in route', async t => {
-    // prep ui5 server runtime config/env files
-    const ui5 = { yaml: path.resolve("./test/auth/ui5-auth-in-yaml.yaml") }
+    //// prep ui5 server runtime config/env files
+    // replace default port 1091 for app router w/ random port
+    await fs.copyFile(path.resolve("./test/auth/ui5-auth-in-yaml.yaml"), `${t.context.tmpDir}/ui5-auth-in-yaml.yaml`) // copy orig ui5.yaml test fixture
+    const _ui5Yaml = await replace({ files: path.resolve(`${t.context.tmpDir}/ui5-auth-in-yaml.yaml`), from: "1091", to: t.context.port.appRouter }) // replace port config in file
+    const ui5 = { yaml: _ui5Yaml[0].file }
     const xsapp = { json: path.resolve("./test/auth/xs-app.json") }
     const defaultEnv = { json: path.resolve("./test/auth/default-env.json") }
 
@@ -52,16 +59,16 @@ test('auth in yaml, xsuaa auth in route', async t => {
     await fs.copy(defaultEnv.json, path.resolve(t.context.tmpDir, "default-env.json"))
 
     // start ui5-app with modified route(s) and config
-    const child = spawn(`ui5 serve --port ${t.context.port} --config ${ui5.yaml}`, {
+    const child = spawn(`ui5 serve --port ${t.context.port.ui5Sserver} --config ${ui5.yaml}`, {
         // stdio: 'inherit', // > don't include stdout in test output
         shell: true,
         cwd: t.context.tmpDir
     })
 
     // wait for ui5 server and app router to boot
-    await waitOn({ resources: [`tcp:${t.context.port}`, "tcp:1091"] })
+    await waitOn({ resources: [`tcp:${t.context.port.ui5Sserver}`, `tcp:${t.context.port.appRouter}`] })
 
-    const app = request(`http://localhost:${t.context.port}`)
+    const app = request(`http://localhost:${t.context.port.ui5Sserver}`)
     // test for the app being started correctly
     const responseIndex = await app.get("/index.html")
     t.is(responseIndex.status, 200, "http 200 on index")
