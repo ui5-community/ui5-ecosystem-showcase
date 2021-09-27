ff3ebd46d0b6d2b6800961d79c7c56bec375fe2e
Volker Buzek <volker.buzek@js-soft.com>
2021-09-27 15:24:41 +0200

    test(zipper): inclusion of additional files

--- packages/ui5-task-zipper/test/zipper.test.js
+++ packages/ui5-task-zipper/test/zipper.test.js
@@ -2,8 +2,42 @@ const crypto = require("crypto");
 const fs = require("fs-extra");
 const path = require("path");
 const { spawnSync } = require("child_process");
+const yauzl = require("yauzl");
 
 const test = require("ava");
+
+/**
+ * check whether a file ("needle") is contained inside the zip
+ *
+ * @param {string} zip path to a zip file
+ * @param {string} needle path/file to find in the zip
+ * @returns {Promise<true|false>} true when needle is found in zip, false otherwise
+ */
+function promisifiedNeedleInHaystack(zip, needle) {
+  return new Promise((resolve, reject) => {
+    let additionalFileFound = false;
+    yauzl.open(zip, { lazyEntries: true }, (err, zipfile) => {
+      if (err) {
+        reject(err);
+      }
+      zipfile.readEntry();
+      zipfile.on("entry", (entry) => {
+        if (entry.fileName.includes(needle)) {
+          resolve(true);
+        } else {
+          zipfile.readEntry();
+        }
+      });
+      zipfile.on("end", () => {
+        if (additionalFileFound) {
+          resolve(true);
+        } else {
+          resolve(false);
+        }
+      });
+    });
+  });
+}
 
 /**
  * copy showcase ui5 app for test purposes
@@ -52,6 +86,7 @@ test("archive creation w/ defaults", async (t) => {
   const targetZip = path.resolve(t.context.tmpDir, "dist", "testSample.zip");
   t.true(await fs.pathExists(targetZip));
 });
+
 test("archive creation with custom archive name", async (t) => {
   const ui5 = {
     yaml: path.resolve("./test/__assets__/ui5.customZipName.yaml"),
@@ -65,6 +100,29 @@ test("archive creation with custom archive name", async (t) => {
   const targetZip = path.resolve(t.context.tmpDir, "dist", "customZipName.zip");
   t.true(await fs.pathExists(targetZip));
 });
-test.todo("additional files");
+
+test("additional files are included in the zip", async (t) => {
+  const ui5 = {
+    yaml: path.resolve("./test/__assets__/ui5.additionalFiles.yaml"),
+  };
+  // create a subdir structure + an (empty) additional file to be included
+  const _addtlFile = "some/sub/dir/additional.html";
+  const additionalFile = path.resolve(t.context.tmpDir, _addtlFile);
+  await fs.ensureFile(additionalFile);
+  spawnSync(`ui5 build --config ${ui5.yaml}`, {
+    stdio: "inherit", // > don't include stdout in test output,
+    shell: true,
+    cwd: t.context.tmpDir,
+  });
+
+  const targetZip = path.resolve(
+    t.context.tmpDir,
+    "dist",
+    "customZipName.zip" // set in ui5.additionalFiles.yaml
+  );
+
+  t.true(await promisifiedNeedleInHaystack(targetZip, _addtlFile));
+});
+
 test.todo("onlyZip");
 test.todo("include dependencies");
