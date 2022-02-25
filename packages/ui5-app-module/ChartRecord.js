cb4f448e6677ec10441dbaeadc7fe459a398cac7
Marcel Schork <marcel.schork@mail.schwarz>
2022-02-25 17:44:59 +0100

    feat: use namespace instead of package name for reusable ui5 modules

--- packages/ui5-app-module/ChartRecord.js
+++ packages/ui5-app-module/ChartRecord.js
@@ -1,6 +1,6 @@
 sap.ui.define(["sap/ui/core/Element"], function(Element) {
 
-    return Element.extend("ui5-app-module.ChartRecord", {
+    return Element.extend("ui5.cc.chart.ChartRecord", {
         metadata: {
             properties: {
                 label: "string",
