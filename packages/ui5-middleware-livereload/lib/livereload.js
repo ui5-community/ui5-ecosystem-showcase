dc3faaf46dbd939610661875ded5d9087ddfa9ac
Peter Muessig <peter.muessig@sap.com>
2022-04-30 17:38:42 +0200

    chore: enable eslint

--- packages/ui5-middleware-livereload/lib/livereload.js
+++ packages/ui5-middleware-livereload/lib/livereload.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars, no-undef */
 const connectLivereload = require("connect-livereload");
 const livereload = require("livereload");
 const path = require("path");
@@ -12,16 +13,16 @@ const portfinder = require("portfinder");
  * @returns {number}
  */
 const getPortForLivereload = async (options, defaultPort) => {
-    if (options.configuration && options.configuration.port) {
-        return options.configuration.port;
-    }
-    try {
-        portfinder.basePort = defaultPort;
-        return await portfinder.getPortPromise();
-    } catch {
-        return defaultPort;
-    }
-}
+	if (options.configuration && options.configuration.port) {
+		return options.configuration.port;
+	}
+	try {
+		portfinder.basePort = defaultPort;
+		return await portfinder.getPortPromise();
+	} catch {
+		return defaultPort;
+	}
+};
 
 /**
  * Custom UI5 Server middleware example
@@ -39,80 +40,77 @@ const getPortForLivereload = async (options, defaultPort) => {
  * @returns {function} Middleware function to use
  */
 module.exports = async ({ resources, options }) => {
-    let port = await getPortForLivereload(options, 35729);
-    let watchPath = "webapp";
-    // due to compatibility reasons we keep the path as watchPath (watchPath has higher precedence than path)
-    if (options.configuration && (options.configuration.watchPath || options.configuration.path)) {
-        watchPath = options.configuration.watchPath || options.configuration.path;
-    }
-    let exclusions = [];
-    let aOptExclusions;
-    if (options.configuration && options.configuration.exclusions) {
-        aOptExclusions = options.configuration.exclusions;
-    }
-    if (options.configuration && aOptExclusions && Array.isArray(aOptExclusions)) {
-        // multilpe exclusions
-        aOptExclusions.forEach((exclusion) => {
-            exclusions.push(new RegExp(exclusion));
-        })
-    } else if (options.configuration && aOptExclusions) {
-        // single exclusion
-        exclusions.push(new RegExp(aOptExclusions));
-    }
-    let extraExts = "xml,json,properties";
-    if (options.configuration && options.configuration.extraExts) {
-        extraExts = options.configuration.extraExts;
-    }
-    let debug = false;
-    if (options.configuration && options.configuration.debug) {
-        debug = options.configuration.debug;
-    }
+	let port = await getPortForLivereload(options, 35729);
+	let watchPath = "webapp";
+	// due to compatibility reasons we keep the path as watchPath (watchPath has higher precedence than path)
+	if (options.configuration && (options.configuration.watchPath || options.configuration.path)) {
+		watchPath = options.configuration.watchPath || options.configuration.path;
+	}
+	let exclusions = [];
+	let aOptExclusions;
+	if (options.configuration && options.configuration.exclusions) {
+		aOptExclusions = options.configuration.exclusions;
+	}
+	if (options.configuration && aOptExclusions && Array.isArray(aOptExclusions)) {
+		// multilpe exclusions
+		aOptExclusions.forEach((exclusion) => {
+			exclusions.push(new RegExp(exclusion));
+		});
+	} else if (options.configuration && aOptExclusions) {
+		// single exclusion
+		exclusions.push(new RegExp(aOptExclusions));
+	}
+	let extraExts = "xml,json,properties";
+	if (options.configuration && options.configuration.extraExts) {
+		extraExts = options.configuration.extraExts;
+	}
+	let debug = false;
+	if (options.configuration && options.configuration.debug) {
+		debug = options.configuration.debug;
+	}
 
-    let serverOptions = {
-        debug: debug,
-        extraExts: extraExts ? extraExts.split(",") : undefined,
-        port: port,
-        exclusions: exclusions
-    };
+	let serverOptions = {
+		debug: debug,
+		extraExts: extraExts ? extraExts.split(",") : undefined,
+		port: port,
+		exclusions: exclusions,
+	};
 
-    const cli = require("yargs");
-    if (cli.argv.h2) {
-        const os = require("os");
-        const fs = require("fs");
+	const cli = require("yargs");
+	if (cli.argv.h2) {
+		const os = require("os");
+		const fs = require("fs");
 
-        sslKeyPath = cli.argv.key ? cli.argv.key : path.join(os.homedir(), ".ui5", "server", "server.key");
-        sslCertPath = cli.argv.cert ? cli.argv.cert : path.join(os.homedir(), ".ui5", "server", "server.crt");
-        debug ? log.info(`Livereload using SSL key ${sslKeyPath}`) : null;
-        debug ? log.info(`Livereload using SSL certificate ${sslCertPath}`) : null;
+		sslKeyPath = cli.argv.key ? cli.argv.key : path.join(os.homedir(), ".ui5", "server", "server.key");
+		sslCertPath = cli.argv.cert ? cli.argv.cert : path.join(os.homedir(), ".ui5", "server", "server.crt");
+		debug ? log.info(`Livereload using SSL key ${sslKeyPath}`) : null;
+		debug ? log.info(`Livereload using SSL certificate ${sslCertPath}`) : null;
 
-        serverOptions.https = {
-            key: fs.readFileSync(sslKeyPath),
-            cert: fs.readFileSync(sslCertPath)
-        };
-    }
+		serverOptions.https = {
+			key: fs.readFileSync(sslKeyPath),
+			cert: fs.readFileSync(sslCertPath),
+		};
+	}
 
-    const livereloadServer = livereload.createServer(
-        serverOptions,
-        () => {
-            log.info("Livereload server started!");
-        }
-    );
+	const livereloadServer = livereload.createServer(serverOptions, () => {
+		log.info("Livereload server started!");
+	});
 
-    if (Array.isArray(watchPath)) {
-        let watchPaths = [];
-        for (let i = 0; i < watchPath.length; i++) {
-            watchPaths.push(path.join(process.cwd(), watchPath[i]));
-        }
-        debug ? log.info(`Livereload connecting to port ${port} for paths ${watchPaths}`) : null;
-        livereloadServer.watch(watchPaths);
-    } else {
-        debug ? log.info(`Livereload connecting to port ${port} for path ${watchPath}`) : null;
-        livereloadServer.watch(path.join(process.cwd(), watchPath));
-    }
+	if (Array.isArray(watchPath)) {
+		let watchPaths = [];
+		for (let i = 0; i < watchPath.length; i++) {
+			watchPaths.push(path.join(process.cwd(), watchPath[i]));
+		}
+		debug ? log.info(`Livereload connecting to port ${port} for paths ${watchPaths}`) : null;
+		livereloadServer.watch(watchPaths);
+	} else {
+		debug ? log.info(`Livereload connecting to port ${port} for path ${watchPath}`) : null;
+		livereloadServer.watch(path.join(process.cwd(), watchPath));
+	}
 
-    // connect-livereload already holds the
-    // method sig (req, res, next)
-    return connectLivereload({
-        port: port
-    });
+	// connect-livereload already holds the
+	// method sig (req, res, next)
+	return connectLivereload({
+		port: port,
+	});
 };
--- packages/ui5-task-minify-xml/lib/minifyXml.js
+++ packages/ui5-task-minify-xml/lib/minifyXml.js
@@ -1,9 +1,7 @@
-/* eslint-env node */
-/* eslint-disable strict */
-
+/* eslint-disable no-unused-vars, no-useless-escape */
 const minify = require("minify-xml").minify;
 const defaultMinifyOptions = {
-    collapseWhitespaceInAttributeValues: true
+	collapseWhitespaceInAttributeValues: true,
 };
 
 const attrValueRegExp = /(?<=<\/?[^\s\/>]+\b(?:\s+[^=\s>]+\s*=\s*(?:"[^"]*"|'[^']*'))*\s+[^\s=\/>]+\s*=\s*(["']))[^\1]*?(?=\1)/g;
@@ -20,37 +18,38 @@ const log = require("@ui5/logger").getLogger("builder:customtask:minifyxml");
  * @param {string} [parameters.options.configuration] Task configuration if given in ui5.yaml
  * @returns {Promise<undefined>} Promise resolving with undefined once data has been written
  */
-module.exports = async ({workspace, dependencies, options}) => {
-    const config = options.configuration || {};
+module.exports = async ({ workspace, dependencies, options }) => {
+	const config = options.configuration || {};
 
-    const fileExtensions = Array.isArray(config.fileExtensions) ?
-        config.fileExtensions : ([config.fileExtensions || "xml"]);
-    const xmlResources = await workspace.byGlob(`**/*.+(${ fileExtensions.join("|")})`);
+	const fileExtensions = Array.isArray(config.fileExtensions) ? config.fileExtensions : [config.fileExtensions || "xml"];
+	const xmlResources = await workspace.byGlob(`**/*.+(${fileExtensions.join("|")})`);
 
-    await Promise.all(xmlResources.map(async resource => {
-        // check if the resource should be excluded from minification
-        if (config.excludePatterns && config.excludePatterns.some(pattern => resource.getPath().includes(pattern))) {
-            return;
-        }
+	await Promise.all(
+		xmlResources.map(async (resource) => {
+			// check if the resource should be excluded from minification
+			if (config.excludePatterns && config.excludePatterns.some((pattern) => resource.getPath().includes(pattern))) {
+				return;
+			}
 
-        // apply our defaults, the defaults of minify-xml will be applied by minify-xml in addition
-        const minifyOptions = {
-            ...defaultMinifyOptions,
-            ...(options?.minifyOptions || {})
-        };
+			// apply our defaults, the defaults of minify-xml will be applied by minify-xml in addition
+			const minifyOptions = {
+				...defaultMinifyOptions,
+				...(options?.minifyOptions || {}),
+			};
 
-        // minify the XML file (fully standard compliant, the resulting XML
-        // file will be semantically equal to the non-minified one)
-        log.info("Minifying file " + resource.getPath());
-        let xml = minify(await resource.getString(), minifyOptions);
+			// minify the XML file (fully standard compliant, the resulting XML
+			// file will be semantically equal to the non-minified one)
+			log.info("Minifying file " + resource.getPath());
+			let xml = minify(await resource.getString(), minifyOptions);
 
-        // collapse multiple spaces in attributes with one space (non-compliant,
-        // but reducing multi-line attributes, such as JSON attributes to one line)
-        if (minifyOptions.collapseWhitespaceInAttributeValues) {
-            xml = xml.replace(attrValueRegExp, value => value.replace(/\s+/g, " "));
-        }
+			// collapse multiple spaces in attributes with one space (non-compliant,
+			// but reducing multi-line attributes, such as JSON attributes to one line)
+			if (minifyOptions.collapseWhitespaceInAttributeValues) {
+				xml = xml.replace(attrValueRegExp, (value) => value.replace(/\s+/g, " "));
+			}
 
-        resource.setString(xml);
-        await workspace.write(resource);
-    }));
+			resource.setString(xml);
+			await workspace.write(resource);
+		})
+	);
 };
