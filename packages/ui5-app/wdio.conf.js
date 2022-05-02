509dad65961180d2c3b89485ae5d20451b76b6c7
Peter Muessig <peter.muessig@sap.com>
2022-05-02 11:38:50 +0200

    chore: re-enable wdio + future error reporting

--- packages/ui5-app/wdio.conf.js
+++ packages/ui5-app/wdio.conf.js
@@ -69,7 +69,7 @@ exports.config = {
 	// Define all options that are relevant for the WebdriverIO instance here
 	//
 	// Level of logging verbosity: trace | debug | info | warn | error | silent
-	logLevel: process.env.HEADLESS ? "silent" : "info",
+	logLevel: process.env.HEADLESS ? "error" : "info",
 	//
 	// Set specific log levels per logger
 	// loggers:
