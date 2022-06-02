8a0e8af328a38236d3f84dc044bda64c978b3df3
Peter Muessig <peter.muessig@sap.com>
2022-06-02 19:46:44 +0200

    feat(ui5-tooling-modules): support pnpm / node_modules symlinks

--- packages/ui5-tooling-modules/lib/util.js
+++ packages/ui5-tooling-modules/lib/util.js
@@ -108,6 +108,7 @@ const that = (module.exports = {
 
 					// create a bundle (maybe in future we should again load the )
 					const bundle = await rollup.rollup({
+						preserveSymlinks: true,
 						input: moduleName,
 						plugins: [
 							(function (options) {
