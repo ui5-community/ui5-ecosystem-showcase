faea158d66dffee1d924c15e6442e1dd634f3945
Peter Muessig <peter.muessig@sap.com>
2022-12-01 18:33:12 +0100

    fix(ui5-tooling-transpile): fix error when no ts file matches for js request
    Fixes: #650

--- packages/ui5-tooling-transpile/lib/middleware.js
+++ packages/ui5-tooling-transpile/lib/middleware.js
@@ -1,8 +1,8 @@
-const babel = require("@babel/core");
-const log = require("@ui5/logger").getLogger("server:custommiddleware:ui5-tooling-transpile");
-const parseurl = require("parseurl");
+const babel = require("@babel/core")
+const log = require("@ui5/logger").getLogger("server:custommiddleware:ui5-tooling-transpile")
+const parseurl = require("parseurl")
 const merge = require("lodash.merge")
-const { createBabelConfig, normalizeLineFeeds } = require("./util");
+const { createBabelConfig, normalizeLineFeeds } = require("./util")
 
 /**
  * Custom middleware to create the UI5 AMD-like bundles for used ES imports from node_modules.
@@ -21,77 +21,73 @@ const { createBabelConfig, normalizeLineFeeds } = require("./util");
  * @param {string} [parameters.options.configuration] Custom server middleware configuration if given in ui5.yaml
  * @returns {function} Middleware function to use
  */
-module.exports = function ({
-    resources,
-    options,
-    middlewareUtil
-}) {
+module.exports = function ({ resources, options /*, middlewareUtil */ }) {
+	const config = options.configuration || {}
 
-    const config = options.configuration || {};
+	const babelConfig = createBabelConfig({ configuration: config, isMiddleware: true })
 
-    const babelConfig = createBabelConfig({configuration: config, isMiddleware: true});
+	let filePatternConfig = config.filePattern
 
-    let filePatternConfig = config.filePattern;
+	if (!filePatternConfig) {
+		filePatternConfig = config.transpileTypeScript ? ".ts" : ".js"
+	}
 
-    if (!filePatternConfig) {
-        filePatternConfig = config.transpileTypeScript ? ".ts" : ".js";
-    }
+	return async (req, res, next) => {
+		if (
+			req.path &&
+			req.path.endsWith(".js") &&
+			!req.path.includes("resources/") &&
+			!(config.excludePatterns || []).some((pattern) => req.path.includes(pattern))
+		) {
+			const pathname = parseurl(req).pathname
+			const pathWithPattern = pathname.replace(".js", filePatternConfig)
+			log.info(`---> ${pathWithPattern}`)
 
-    return async (req, res, next) => {
+			try {
+				// grab the file via @ui5/fs.AbstractReader API
+				const matchedResources = await resources.rootProject.byGlob(pathWithPattern)
+				config.debug && log.info(`handling ${req.path}...`)
 
-        if (
-            req.path &&
-            req.path.endsWith(".js") &&
-            !req.path.includes("resources/") &&
-            !(config.excludePatterns || []).some((pattern) => req.path.includes(pattern))
-        ) {
+				if (!matchedResources || !matchedResources.length) {
+					const fileNotFoundError = new Error(`No match found for ${req.path}`)
+					fileNotFoundError.code = 404
+					fileNotFoundError.file = pathWithPattern
+					throw fileNotFoundError
+				}
 
-            const pathname = parseurl(req).pathname;
-            const pathWithPattern = pathname.replace(".js", filePatternConfig);
-            log.info(`---> ${pathWithPattern}`);
+				// prefer js over other extensions, otherwise grab first possible path
+				const resource = matchedResources.find((r) => r.getPath() === pathname) || matchedResources[0]
+				if (matchedResources.length > 1) {
+					log.warn(
+						`found more than 1 file for given pattern (${filePatternConfig}): ${matchedResources
+							.map((r) => r.getPath())
+							.join(", ")} `
+					)
+					log.info(`using: ${resource.getPath()}`)
+				}
 
-            try {
+				// read file into string
+				const source = await resource.getString()
 
-                // grab the file via @ui5/fs.AbstractReader API
-                const matchedResources = await  resources.rootProject.byGlob(pathWithPattern);
-                config.debug && log.info(`handling ${req.path}...`);
+				config.debug ? log.info(`...${pathWithPattern} transpiled!`) : null
+				const babelConfigForFile = merge({}, babelConfig, {
+					filename: pathWithPattern // necessary for source map <-> source assoc
+				})
+				let result = await babel.transformAsync(source, babelConfigForFile)
 
-                if (!matchedResources || !matchedResources.length) {
-                    fileNotFoundError.file = pathWithPattern;
-                    throw fileNotFoundError;
-                }
-
-                // prefer js over other extensions, otherwise grab first possible path
-                const resource = matchedResources.find((r) => r.getPath() === pathname) || matchedResources[0]
-                if (matchedResources.length > 1) {
-                    log.warn(`found more than 1 file for given pattern (${filePatternConfig}): ${matchedResources.map((r) => r.getPath()).join(", ")} `);
-                    log.info(`using: ${resource.getPath()}`);
-                }
-
-                // read file into string
-                const source = await resource.getString();
-
-                config.debug ? log.info(`...${pathWithPattern} transpiled!`) : null
-                const babelConfigForFile = merge({}, babelConfig, {
-                    filename: pathWithPattern // necessary for source map <-> source assoc
-                })
-                let result = await babel.transformAsync(source, babelConfigForFile);
-
-                // send out transpiled source + source map
-                res.type(".js");
-                res.end(normalizeLineFeeds(result.code));
-
-            } catch(err) {
-                if (err.code === 404) {
-                    log.warn(`...file not found: ${err.file}!`)
-                } else {
-                    log.error(err)
-                }
-				next();
-            }
-        }
-		else {
-			next();
+				// send out transpiled source + source map
+				res.type(".js")
+				res.end(normalizeLineFeeds(result.code))
+			} catch (err) {
+				if (err.code === 404) {
+					log.warn(`...file not found: ${err.file}!`)
+				} else {
+					log.error(err)
+				}
+				next()
+			}
+		} else {
+			next()
 		}
-    }
+	}
 }
