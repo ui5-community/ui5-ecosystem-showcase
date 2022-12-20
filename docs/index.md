9f854ec45f514664a9b25e85137553877c52577a
Peter Muessig <petermuessig@users.noreply.github.com>
2022-12-20 21:45:19 +0100

    feat(ui5-tooling-transpile): enable config files (#660)
    * feat(ui5-tooling-transpile): enable config files
    
    * chore: update dependencies

--- docs/index.md
+++ docs/index.md
@@ -32,7 +32,6 @@ The following tasks (known to this page) have been created and are available in 
 | [ui5-task-pwa-enabler](https://www.npmjs.com/package/ui5-task-pwa-enabler) | enables ui5 app with pwa functionalities | [![npm version](https://badge.fury.io/js/ui5-task-pwa-enabler.svg)](https://badge.fury.io/js/ui5-task-pwa-enabler) |
 | [ui5-task-stringreplacer](https://www.npmjs.com/package/ui5-task-stringreplacer) | replaces placeholder strings | [![npm version](https://badge.fury.io/js/ui5-task-stringreplacer.svg)](https://badge.fury.io/js/ui5-task-stringreplacer) |
 | [ui5-task-transpile-gen](https://www.npmjs.com/package/ui5-task-transpile-gen) | transpile es6 to es5 code including Async Await syntax | [![npm version](https://badge.fury.io/js/ui5-task-transpile-gen.svg)](https://badge.fury.io/js/ui5-task-transpile-gen) |
-| [ui5-task-transpile](https://www.npmjs.com/package/ui5-task-transpile) | transpile es6 to es5 code | [![npm version](https://badge.fury.io/js/ui5-task-transpile.svg)](https://badge.fury.io/js/ui5-task-transpile) |
 | [ui5-task-zipper](https://www.npmjs.com/package/ui5-task-zipper) | bundle the entire webapp in a zip-archive | [![npm version](https://badge.fury.io/js/ui5-task-zipper.svg)](https://badge.fury.io/js/ui5-task-zipper) |
 | [ui5-task-cachebuster-indexing](https://www.npmjs.com/package/ui5-task-cachebuster-indexing) | task for indexing files to enable CacheBuster | [![npm version](https://badge.fury.io/js/ui5-task-cachebuster-indexing.svg)](https://badge.fury.io/js/ui5-task-cachebuster-indexing) |
 
@@ -51,7 +50,6 @@ The following middlewares (known to this page) have been created and are availab
 | [ui5-middleware-index](packages/ui5-middleware-index/README.md) | serve an HTML file for / (root) | [![npm version](https://badge.fury.io/js/ui5-middleware-index.svg)](https://badge.fury.io/js/ui5-middleware-index) |
 | [ui5-middleware-livecompileless](https://www.npmjs.com/package/ui5-middleware-livecompileless) | livecompiling of less files in the app folder | [![npm version](https://badge.fury.io/js/ui5-middleware-livecompileless.svg)](https://badge.fury.io/js/ui5-middleware-livecompileless) |
 | [ui5-middleware-livereload](https://www.npmjs.com/package/ui5-middleware-livereload) | usage of livereload for development | [![npm version](https://badge.fury.io/js/ui5-middleware-livereload.svg)](https://badge.fury.io/js/ui5-middleware-livereload) |
-| [ui5-middleware-livetranspile](https://www.npmjs.com/package/ui5-middleware-livetranspile) | on-demand es6 to es5 transpile when requesting js | [![npm version](https://badge.fury.io/js/ui5-middleware-livetranspile.svg)](https://badge.fury.io/js/ui5-middleware-livetranspile) |
 | [ui5-middleware-onelogin](https://www.npmjs.com/package/ui5-middleware-onelogin) | enable a generic login support | [![npm version](https://badge.fury.io/js/ui5-middleware-onelogin.svg)](https://badge.fury.io/js/ui5-middleware-onelogin) |
 | [ui5-middleware-route-proxy](https://www.npmjs.com/package/ui5-middleware-route-proxy) | Proxy to forward request for a specific route | [![npm version](https://badge.fury.io/js/ui5-middleware-route-proxy.svg)](https://badge.fury.io/js/ui5-middleware-route-proxy) |
 | [ui5-middleware-servestatic](https://www.npmjs.com/package/ui5-middleware-servestatic) | serve static resources | [![npm version](https://badge.fury.io/js/ui5-middleware-servestatic.svg)](https://badge.fury.io/js/ui5-middleware-servestatic) |
@@ -68,5 +66,6 @@ The following tooling extensions (known to this page) have been created and are 
 | NPM package | Description | Badge |
 | ----------- | ----------- | ----- |
 | [ui5-tooling-modules](https://www.npmjs.com/package/ui5-tooling-modules) | direct consumption of NPM packages | [![npm version](https://badge.fury.io/js/ui5-tooling-modules.svg)](https://badge.fury.io/js/ui5-tooling-modules) |
+| [ui5-tooling-transpile](https://www.npmjs.com/package/ui5-tooling-transpile) | transpile resources using Babel | [![npm version](https://badge.fury.io/js/ui5-tooling-transpile.svg)](https://badge.fury.io/js/ui5-tooling-transpile) |
 
 There might be more tooling extensions in the public NPM registry. You can find all middlewares which applied the suggested naming convention at [https://www.npmjs.com/search?q=ui5-tooling-](https://www.npmjs.com/search?q=ui5-tooling-).
--- packages/ui5-tooling-transpile/lib/task.js
+++ packages/ui5-tooling-transpile/lib/task.js
@@ -1,10 +1,11 @@
 const log = require("@ui5/logger").getLogger("builder:customtask:ui5-tooling-transpile");
 const resourceFactory = require("@ui5/fs").resourceFactory;
 const { createBabelConfig, normalizeLineFeeds } = require("./util");
+const path = require("path");
 const babel = require("@babel/core");
 
 /**
- * Custom task to create the UI5 AMD-like bundles for used ES imports from node_modules.
+ * Custom task to transpile resources to JavaScript modules.
  *
  * @param {object} parameters Parameters
  * @param {module:@ui5/fs.DuplexCollection} parameters.workspace DuplexCollection to read and write files
@@ -17,57 +18,67 @@ const babel = require("@babel/core");
  * @param {string} [parameters.options.configuration] Task configuration if given in ui5.yaml
  * @returns {Promise<undefined>} Promise resolving with <code>undefined</code> once data has been written
  */
-module.exports = async function ({
-    workspace,
-    dependencies,
-    taskUtil,
-    options
-}) {
-    const config = options.configuration || {};
+module.exports = async function ({ workspace, dependencies, /* taskUtil,*/ options }) {
+	const config = options?.configuration || {};
+	config.includes = config.includes || config.includePatterns;
+	config.excludes = config.excludes || config.excludePatterns;
 
-    const babelConfig = createBabelConfig({configuration: config, isMiddleware: false});
+	const babelConfig = await createBabelConfig({ configuration: config, isMiddleware: false });
 
-    let filePatternConfig = config.filePattern;
+	let filePatternConfig = config.filePattern; // .+(ts|tsx)
+	if (!filePatternConfig) {
+		filePatternConfig = config.transpileTypeScript ? ".ts" : ".js";
+	}
 
-    if (!filePatternConfig) {
-        filePatternConfig = config.transpileTypeScript ? ".ts" : ".js";
-    }
+	// TODO: should we accept the full glob pattern as param or just the file pattern?
+	const allResources = await workspace.byGlob(`/**/*${filePatternConfig}`);
+	if (config.transpileDependencies) {
+		// TODO: does transpileDependencies make sense for JavaScript files?
+		const depsResources = await dependencies.byGlob(`/**/*${filePatternConfig}`);
+		allResources.push(...depsResources);
+	}
 
-    //TODO: should we accept the full glob pattern as param or just the file pattern?
-    const allResources = await workspace.byGlob(`/**/*${filePatternConfig}`);
+	await Promise.all(
+		allResources.map(async (resource) => {
+			const resourcePath = resource.getPath();
 
-    await Promise.all(allResources.map(async (resource) => {
-        // Ignore files that match the excludePatterns configuration
-        if (!(config.excludePatterns || []).some(pattern => resource.getPath().includes(pattern))) {
-            const filePath = resource.getPath().replace(new RegExp("\\.[^.]+$"), ".js");
+			// Ignore files that match the excludePatterns configuration
+			if (
+				!(config.excludes || []).some((pattern) => resourcePath.includes(pattern)) ||
+				(config.includes || []).some((pattern) => resourcePath.includes(pattern))
+			) {
+				const filePath = resourcePath.replace(new RegExp("\\.[^.]+$"), ".js");
 
-            const transpileResult = await resource.getString().then((value) => {
-                config.debug && log.info("Transpiling file " + resource.getPath());
+				// read file into string
+				const source = await resource.getString();
 
-                // add source file name
-                babelConfig.filename = resource.getPath();
+				// transpile the source
+				config.debug && log.info(`Transpiling resource ${resource.getPath()}`);
+				const result = await babel.transformAsync(
+					source,
+					Object.assign({}, babelConfig, {
+						filename: resource.getPath() // necessary for source map <-> source assoc
+					})
+				);
 
-                return babel.transformAsync(value, babelConfig);
-            });
+				resource.setString(normalizeLineFeeds(result.code));
+				resource.setPath(filePath);
+				workspace.write(resource);
 
-            const code = normalizeLineFeeds(transpileResult.code);
-            resource.setString(code);
-            resource.setPath(filePath);
-			workspace.write(resource);
+				// create SourceMap resource if available
+				if (result.map) {
+					//add the transpiled file name
+					result.map.file = path.basename(filePath);
+					config.debug && log.info(`  + sourcemap ${filePath}.map`);
 
-            // Create SourceMap File if requested
-            if (transpileResult.map) {
-                const filename = filePath.split("/").pop();
-                //add the transpiled file name
-                transpileResult.map.file = filename;
+					const resourceMap = resourceFactory.createResource({
+						path: `${filePath}.map`,
+						string: JSON.stringify(result.map)
+					});
 
-                const resourceMap = resourceFactory.createResource({
-                    path: `${filePath}.map`,
-                    string: JSON.stringify(transpileResult.map)
-                });
-
-                workspace.write(resourceMap);
-            }
-        }
-    }));
+					workspace.write(resourceMap);
+				}
+			}
+		})
+	);
 };
