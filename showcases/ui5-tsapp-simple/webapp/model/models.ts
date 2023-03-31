8dba327c4d73a3c7781385d63266f022aef1f120
Peter Muessig <peter.muessig@sap.com>
2023-03-31 22:55:40 +0200

    chore: removal of experimental commit

--- showcases/ui5-tsapp-simple/webapp/model/models.ts
+++ showcases/ui5-tsapp-simple/webapp/model/models.ts
@@ -1,21 +1,8 @@
 import JSONModel from "sap/ui/model/json/JSONModel";
 import Device from "sap/ui/Device";
-import { TypeSafeJsonModel } from "ui5-tsutil/lib/TypeSafeJSONModel";
 
 export function createDeviceModel(): JSONModel {
 	const model = new JSONModel(Device);
 	model.setDefaultBindingMode("OneWay");
 	return model;
-}
-
-/**
- * JSON safe object
- */
-type JSONObject = {
-	[x: string]: JSONObject;
-};
-
-export function createTypedJSONModel(): TypeSafeJsonModel<JSONObject> {
-	const model = new JSONModel();
-	return model as TypeSafeJsonModel<JSONObject>;
 }
