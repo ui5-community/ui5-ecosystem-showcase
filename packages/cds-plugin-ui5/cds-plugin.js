9ba7579188de57754aa28433b5751fd51eaa6a34
Peter Muessig <peter.muessig@sap.com>
2023-09-10 08:26:06 +0200

    fix(cds-plugin-ui5): rewrite UI5 server listing also in approuter

--- packages/cds-plugin-ui5/cds-plugin.js
+++ packages/cds-plugin-ui5/cds-plugin.js
@@ -5,6 +5,7 @@ const log = require("./lib/log");
 const findUI5Modules = require("./lib/findUI5Modules");
 const createPatchedRouter = require("./lib/createPatchedRouter");
 const applyUI5Middleware = require("./lib/applyUI5Middleware");
+const rewriteHTML = require("./lib/rewriteHTML");
 
 // marker that the cds-plugin-ui5 plugin is running
 // to disable the ui5-middleware-cap if used in apps
@@ -67,18 +68,20 @@ cds.on("bootstrap", async function bootstrap(app) {
 	if (links.length > 0) {
 		// register the custom middleware (similar like in @sap/cds/server.js)
 		app.get("/", function appendLinksToIndex(req, res, next) {
-			const send = res.send;
-			res.send = function (content) {
-				// the first <ul> element contains the links to the
-				// application pages which is fully under control of
-				// our plugin now and we keep all links to static
-				// pages to ensure coop with classic apps
-				const HTMLParser = require("node-html-parser");
-				const doc = new HTMLParser.parse(content);
-				const head = doc.getElementsByTagName("head")?.[0];
-				if (head) {
-					head.appendChild(
-						HTMLParser.parse(`<style>
+			req._cds_plugin_ui5 = true; // marker for patched router to ignore
+			rewriteHTML(
+				req,
+				res,
+				() => true,
+				(doc) => {
+					// the first <ul> element contains the links to the
+					// application pages which is fully under control of
+					// our plugin now and we keep all links to static
+					// pages to ensure coop with classic apps
+					const head = doc.getElementsByTagName("head")?.[0];
+					head?.insertAdjacentHTML(
+						"beforeend",
+						`<style>
 					a.ui5:after {
 						content: "UI5";
 						font-weight: bold;
@@ -92,27 +95,25 @@ cds.on("bootstrap", async function bootstrap(app) {
 							color: #FF5A37;
 						}
 					}
-					</style>`)
+					</style>`
 					);
+					const ul = doc.getElementsByTagName("ul")?.[0];
+					if (ul) {
+						const newLis = [];
+						const lis = ul.getElementsByTagName("li");
+						lis?.forEach((li) => {
+							const appDir = li.firstChild?.text?.split("/")?.[1];
+							if (localApps.has(appDir)) {
+								newLis.push(li.toString());
+							}
+						});
+						newLis.push(...links.map((link) => `<li><a class="ui5" href="${link}">${link}</a></li>`));
+						ul.innerHTML = newLis.join("\n");
+					} else {
+						log.warn(`Failed to inject application links into CAP index page!`);
+					}
 				}
-				const ul = doc.getElementsByTagName("ul")?.[0];
-				if (ul) {
-					const newLis = [];
-					const lis = ul.getElementsByTagName("li");
-					lis?.forEach((li) => {
-						const appDir = li.firstChild?.text?.split("/")?.[1];
-						if (localApps.has(appDir)) {
-							newLis.push(li.toString());
-						}
-					});
-					newLis.push(...links.map((link) => `<li><a class="ui5" href="${link}">${link}</a></li>`));
-					ul.innerHTML = newLis.join("\n");
-					content = doc.toString();
-				} else {
-					log.warn(`Failed to inject application links into CAP index page!`);
-				}
-				send.apply(this, arguments);
-			};
+			);
 			next();
 		});
 
