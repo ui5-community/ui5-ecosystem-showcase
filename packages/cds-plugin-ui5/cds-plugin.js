fc7b9b5e8c362a499e2fbb70ece764e9f17b0f6e
Peter Muessig <petermuessig@users.noreply.github.com>
2023-09-15 12:41:20 +0200

    fix: no cache for rewrite, cds server lookup (#843)

--- packages/cds-plugin-ui5/cds-plugin.js
+++ packages/cds-plugin-ui5/cds-plugin.js
@@ -11,142 +11,148 @@ const rewriteHTML = require("./lib/rewriteHTML");
 // to disable the ui5-middleware-cap if used in apps
 process.env["cds-plugin-ui5"] = true;
 
-// promise to await the bootstrap and lock the
-// served event to delay the startup a bit
-let bootstrapped;
-const bootstrapCompleted = new Promise((r) => {
-	bootstrapped = r;
-});
+// identify whether the execution should be skipped
+let skip = false;
+if (process.env["ui5-middleware-cap"]) {
+	log.info("Skip execution of plugin because is has been started via ui5-middleware-cap!");
+	skip = true;
+} else if (process.env["dev-approuter"]) {
+	log.info("Skip execution of plugin because is has been started via dev-approuter!");
+	skip = true;
+}
 
-// hook into the "served" event to delay the startup of the
-// CDS server until the bootstrap is completed and the UI5
-// middlewares for the UI5 applications are properly available
-cds.on("served", async function served(/* cdsServices */) {
-	log.debug("served");
-	await bootstrapCompleted;
-});
+// only hook into lifecycle if the plugin should not be skipped
+if (!skip) {
+	// promise to await the bootstrap and lock the
+	// served event to delay the startup a bit
+	let bootstrapped;
+	const bootstrapCompleted = new Promise((r) => {
+		bootstrapped = r;
+	});
 
-// hook into the "bootstrap" event to startup the UI5 middlewares
-// for the available UI5 applications in the CDS server and its deps
-cds.on("bootstrap", async function bootstrap(app) {
-	log.debug("bootstrap");
+	// hook into the "served" event to delay the startup of the
+	// CDS server until the bootstrap is completed and the UI5
+	// middlewares for the UI5 applications are properly available
+	cds.on("served", async function served(/* cdsServices */) {
+		log.debug("served");
+		await bootstrapCompleted;
+	});
 
-	const cwd = process.cwd();
-	const ui5Modules = await findUI5Modules({ cwd });
-	const { localApps, config } = ui5Modules;
+	// hook into the "bootstrap" event to startup the UI5 middlewares
+	// for the available UI5 applications in the CDS server and its deps
+	cds.on("bootstrap", async function bootstrap(app) {
+		log.debug("bootstrap");
 
-	const links = [];
+		const cwd = process.cwd();
+		const ui5Modules = await findUI5Modules({ cwd });
+		const { localApps, config } = ui5Modules;
 
-	// register the UI5 modules via their own router/middlewares
-	for await (const ui5Module of ui5Modules) {
-		const { moduleId, mountPath, modulePath } = ui5Module;
+		const links = [];
 
-		// mounting the Router for the UI5 application to the CDS server
-		log.info(`Mounting ${mountPath} to UI5 app ${modulePath} (id=${moduleId})${config[moduleId] ? ` using config=${JSON.stringify(config[moduleId])}` : ""}`);
+		// register the UI5 modules via their own router/middlewares
+		for await (const ui5Module of ui5Modules) {
+			const { moduleId, mountPath, modulePath } = ui5Module;
 
-		// create a patched router
-		const router = await createPatchedRouter();
+			// mounting the Router for the UI5 application to the CDS server
+			log.info(`Mounting ${mountPath} to UI5 app ${modulePath} (id=${moduleId})${config[moduleId] ? ` using config=${JSON.stringify(config[moduleId])}` : ""}`);
 
-		// apply the UI5 middlewares to the router and
-		// retrieve the available HTML pages
-		const appInfo = await applyUI5Middleware(router, {
-			cwd,
-			basePath: modulePath,
-			...(config[moduleId] || {}),
-		});
+			// create a patched router
+			const router = await createPatchedRouter();
 
-		// register the router to the specified mount path
-		app.use(mountPath, router);
+			// apply the UI5 middlewares to the router and
+			// retrieve the available HTML pages
+			const appInfo = await applyUI5Middleware(router, {
+				cwd,
+				basePath: modulePath,
+				...(config[moduleId] || {}),
+			});
 
-		// append the HTML pages to the links
-		appInfo.pages.forEach((page) => {
-			const prefix = mountPath !== "/" ? mountPath : "";
-			links.push(`${prefix}${page}`);
-		});
-	}
+			// register the router to the specified mount path
+			app.use(mountPath, router);
 
-	// identify whether the welcome page should be rewritten
-	let rewrite = links.length > 0;
-	if (process.env["ui5-middleware-cap"]) {
-		log.info("Skip rewriting of welcome page as CDS server is started via ui5-middleware-cap!");
-		rewrite = false;
-	} else if (process.env["dev-approuter"]) {
-		log.info("Skip rewriting of welcome page as CDS server is started via dev-approuter!");
-		rewrite = false;
-	}
+			// append the HTML pages to the links
+			appInfo.pages.forEach((page) => {
+				const prefix = mountPath !== "/" ? mountPath : "";
+				links.push(`${prefix}${page}`);
+			});
+		}
 
-	// rewrite the welcome page
-	if (rewrite) {
-		// register the custom middleware (similar like in @sap/cds/server.js)
-		app.get("/", function appendLinksToIndex(req, res, next) {
-			req._cds_plugin_ui5 = true; // marker for patched router to ignore
-			rewriteHTML(
-				req,
-				res,
-				() => true,
-				(doc) => {
-					// the first <ul> element contains the links to the
-					// application pages which is fully under control of
-					// our plugin now and we keep all links to static
-					// pages to ensure coop with classic apps
-					const head = doc.getElementsByTagName("head")?.[0];
-					head?.insertAdjacentHTML(
-						"beforeend",
-						`<style>
-					a.ui5:after {
-						content: "UI5";
-						font-weight: bold;
-						font-size: 0.5rem;
-						vertical-align: super;
-						margin: 0.25rem;
-						color: #1873B4;
-					}
-					@media (prefers-color-scheme: dark) {
+		// identify whether the welcome page should be rewritten
+		let rewrite = links.length > 0;
+
+		// rewrite the welcome page
+		if (rewrite) {
+			// register the custom middleware (similar like in @sap/cds/server.js)
+			app.get("/", function appendLinksToIndex(req, res, next) {
+				req._cds_plugin_ui5 = true; // marker for patched router to ignore
+				rewriteHTML(
+					req,
+					res,
+					() => true,
+					(doc) => {
+						// the first <ul> element contains the links to the
+						// application pages which is fully under control of
+						// our plugin now and we keep all links to static
+						// pages to ensure coop with classic apps
+						const head = doc.getElementsByTagName("head")?.[0];
+						head?.insertAdjacentHTML(
+							"beforeend",
+							`<style>
 						a.ui5:after {
-							color: #FF5A37;
+							content: "UI5";
+							font-weight: bold;
+							font-size: 0.5rem;
+							vertical-align: super;
+							margin: 0.25rem;
+							color: #1873B4;
+						}
+						@media (prefers-color-scheme: dark) {
+							a.ui5:after {
+								color: #FF5A37;
+							}
+						}
+						</style>`
+						);
+						const ul = doc.getElementsByTagName("ul")?.[0];
+						if (ul) {
+							const newLis = [];
+							const lis = ul.getElementsByTagName("li");
+							lis?.forEach((li) => {
+								const appDir = li.firstChild?.text?.split("/")?.[1];
+								if (localApps.has(appDir)) {
+									newLis.push(li.toString());
+								}
+							});
+							newLis.push(...links.sort().map((link) => `<li><a class="ui5" href="${link}">${link}</a></li>`));
+							ul.innerHTML = newLis.join("\n");
+						} else {
+							log.warn(`Failed to inject application links into CDS index page!`);
 						}
 					}
-					</style>`
-					);
-					const ul = doc.getElementsByTagName("ul")?.[0];
-					if (ul) {
-						const newLis = [];
-						const lis = ul.getElementsByTagName("li");
-						lis?.forEach((li) => {
-							const appDir = li.firstChild?.text?.split("/")?.[1];
-							if (localApps.has(appDir)) {
-								newLis.push(li.toString());
-							}
-						});
-						newLis.push(...links.map((link) => `<li><a class="ui5" href="${link}">${link}</a></li>`));
-						ul.innerHTML = newLis.join("\n");
-					} else {
-						log.warn(`Failed to inject application links into CDS index page!`);
-					}
-				}
-			);
-			next();
-		});
+				);
+				next();
+			});
 
-		// move our middleware before the CDS index serve middleware to
-		// allow that we can intercept the response and modify it to
-		// inject our application pages an remove the exitsing ones
-		const middlewareStack = app?._router?.stack;
-		if (Array.isArray(middlewareStack)) {
-			const cmw = middlewareStack.pop();
-			const idxOfServeStatic = middlewareStack.findIndex((layer) => layer.name === "serveStatic");
-			if (idxOfServeStatic !== -1) {
-				middlewareStack.splice(idxOfServeStatic, 0, cmw);
+			// move our middleware before the CDS index serve middleware to
+			// allow that we can intercept the response and modify it to
+			// inject our application pages an remove the exitsing ones
+			const middlewareStack = app?._router?.stack;
+			if (Array.isArray(middlewareStack)) {
+				const cmw = middlewareStack.pop();
+				const idxOfServeStatic = middlewareStack.findIndex((layer) => layer.name === "serveStatic");
+				if (idxOfServeStatic !== -1) {
+					middlewareStack.splice(idxOfServeStatic, 0, cmw);
+				} else {
+					log.error(`Failed to determine welcome page middleware! The links of the application pages may not work properly!`);
+				}
 			} else {
-				log.error(`Failed to determine welcome page middleware! The links of the application pages may not work properly!`);
+				log.error(`Failed to inject application pages to welcome page! The links of the application pages may not work properly!`);
 			}
-		} else {
-			log.error(`Failed to inject application pages to welcome page! The links of the application pages may not work properly!`);
 		}
-	}
-	// bootstrap completed, unlock the "served" event
-	bootstrapped();
-});
+		// bootstrap completed, unlock the "served" event
+		bootstrapped();
+	});
+}
 
 // return callback for plugin activation
 module.exports = {
