ee0df6b1cd2242c3a0d49bda4737838f17daef33
Peter Muessig <petermuessig@users.noreply.github.com>
2023-10-01 12:32:26 +0200

    fix(cds-plugin-ui5): handle dirs without trailing slash (#856)

--- packages/cds-plugin-ui5/lib/createPatchedRouter.js
+++ packages/cds-plugin-ui5/lib/createPatchedRouter.js
@@ -20,24 +20,23 @@ module.exports = async function createPatchedRouter() {
 		// rewite the path to simulate requests on the root level
 		req.originalUrl = req.url;
 		req.baseUrl = "/";
+		// only accept requests for html-related content (via accept header)
+		const accept = req.headers["accept"]?.indexOf("html") !== -1;
 		// disable the compression when livereload is used
 		// for loading html-related content (via accept header)
 		// otherwise we run into compression issue with CDS livereload
-		const accept = req.headers["accept"]?.indexOf("html") !== -1;
 		if (accept && res._livereload) {
 			req.headers["accept-encoding"] = "identity";
 		}
-		// override UI5 server directory listing if:
-		//   1.) not handled by the CDS Plugin UI5 already
-		//   2.) only if it ends with a slash
-		//   3.) not forwarded to a welcome page
-		if (!req._cds_plugin_ui5 && req.url?.endsWith("/") && req.url === (req?.["ui5-middleware-index"]?.url || req.url)) {
+		// override UI5 server directory listing if not forwarded to a welcome page
+		// and not already handled by the HTML rewriter of the CDS welcome page
+		if (accept && !req._cds_plugin_ui5 && req.url === (req["ui5-middleware-index"]?.url || req.url)) {
 			// determine context path (approuter contains x-forwarded-path header)
 			let contextPath = baseUrl;
-			if (req.headers["x-forwarded-path"]) {
+			if (req.headers["x-forwarded-path"]?.endsWith(url)) {
 				// determine the context path by removing the subpath from the forwarded path
 				contextPath = req.headers["x-forwarded-path"].slice(0, -1 * url.length);
-			} else if (req["ui5-patched-router"].originalUrl) {
+			} else if (req["ui5-patched-router"].originalUrl?.endsWith(url)) {
 				// determine the context path by removing the subpath from the originalUrl
 				contextPath = req["ui5-patched-router"].originalUrl.slice(0, -1 * url.length);
 			}
@@ -50,21 +49,21 @@ module.exports = async function createPatchedRouter() {
 				},
 				(doc) => {
 					const title = doc.getElementsByTagName("title")?.[0];
-					if (title) {
+					if (title && title.innerHTML.startsWith(`Index of ${req.url}`)) {
 						title.innerHTML = `Index of ${contextPath}/`;
+						const files = doc.getElementById("files");
+						const filesas = files?.getElementsByTagName("a");
+						filesas?.forEach((a) => {
+							a.setAttribute("href", `${contextPath}${a.getAttribute("href")}`);
+						});
+						const h1 = doc.getElementsByTagName("h1")?.[0];
+						const h1as = h1?.getElementsByTagName("a");
+						h1as?.forEach((a) => {
+							const path = a.getAttribute("href") === "/" ? "/" : a.getAttribute("href") + "/";
+							a.setAttribute("href", `${contextPath}${path}`);
+						});
+						h1?.insertAdjacentHTML("afterbegin", `<a href="/">🏡</a> / `);
 					}
-					const files = doc.getElementById("files");
-					const filesas = files?.getElementsByTagName("a");
-					filesas?.forEach((a) => {
-						a.setAttribute("href", `${contextPath}${a.getAttribute("href")}`);
-					});
-					const h1 = doc.getElementsByTagName("h1")?.[0];
-					const h1as = h1?.getElementsByTagName("a");
-					h1as?.forEach((a) => {
-						const path = a.getAttribute("href") === "/" ? "/" : a.getAttribute("href") + "/";
-						a.setAttribute("href", `${contextPath}${path}`);
-					});
-					h1?.insertAdjacentHTML("afterbegin", `<a href="/">🏡</a> / `);
 				}
 			);
 		}
