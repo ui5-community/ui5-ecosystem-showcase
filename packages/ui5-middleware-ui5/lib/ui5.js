905bcd15c71ada713b8b90b0d333d74eda011a86
Peter Muessig <petermuessig@users.noreply.github.com>
2023-10-03 22:17:57 +0200

    chore(ui5-middleware-ui5): remove not needed middleware function (#870)

--- packages/ui5-middleware-ui5/lib/ui5.js
+++ packages/ui5-middleware-ui5/lib/ui5.js
@@ -31,34 +31,28 @@ module.exports = async ({ log, options, middlewareUtil }) => {
 	if (process.env["cds-plugin-ui5"]) {
 		log.info("Skip middleware as the UI5 application has been started embedded in the CDS server!");
 	} else {
-		return hook(
-			"ui5-middleware-ui5",
-			async ({ use }) => {
-				const ui5Modules = await findUI5Modules({
+		return hook("ui5-middleware-ui5", async ({ use }) => {
+			const ui5Modules = await findUI5Modules({
+				cwd,
+				log,
+				config,
+			});
+			for await (const ui5Module of ui5Modules) {
+				const { moduleId, mountPath, modulePath } = ui5Module;
+				const options = config?.modules?.[moduleId];
+
+				// mounting the Router for the UI5 application to the CDS server
+				log.info(`Mounting ${mountPath} to UI5 app ${modulePath} (id=${moduleId})${options ? ` using options=${JSON.stringify(options)}` : ""}`);
+
+				const router = await createPatchedRouter();
+				await applyUI5Middleware(router, {
 					cwd,
-					log,
-					config,
+					basePath: modulePath,
+					...(options || {}),
 				});
-				for await (const ui5Module of ui5Modules) {
-					const { moduleId, mountPath, modulePath } = ui5Module;
-					const options = config?.modules?.[moduleId];
-
-					// mounting the Router for the UI5 application to the CDS server
-					log.info(`Mounting ${mountPath} to UI5 app ${modulePath} (id=${moduleId})${options ? ` using options=${JSON.stringify(options)}` : ""}`);
-
-					const router = await createPatchedRouter();
-					await applyUI5Middleware(router, {
-						cwd,
-						basePath: modulePath,
-						...(options || {}),
-					});
-					use(mountPath, router);
-				}
-			},
-			function (req, res, next) {
-				/* dummy middleware function */ next();
+				use(mountPath, router);
 			}
-		);
+		});
 	}
 
 	// in any case, at least register a dummy middleware function
