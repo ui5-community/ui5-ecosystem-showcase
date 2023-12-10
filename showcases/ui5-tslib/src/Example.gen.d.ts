1fe4eb39b81c05e91193ac7cc94e213260630027
Peter Muessig <peter.muessig@sap.com>
2023-12-10 12:54:30 +0100

    fix(ui5-tooling-transpile): more usage of UI5 Tooling V3 APIs

--- showcases/ui5-tslib/src/Example.gen.d.ts
+++ showcases/ui5-tslib/src/Example.gen.d.ts
@@ -1,132 +1,91 @@
-import { ExampleColor } from "ui5/ecosystem/demo/tslib/library";
 import Event from "sap/ui/base/Event";
+import { ExampleColor } from "ui5/ecosystem/demo/tslib/library";
 import { PropertyBindingInfo } from "sap/ui/base/ManagedObject";
 import { $ControlSettings } from "sap/ui/core/Control";
 
 declare module "./Example" {
-	/**
-	 * Interface defining the settings object used in constructor calls
-	 */
-	interface $ExampleSettings extends $ControlSettings {
-		/**
-		 * The text to display.
-		 */
-		text?: string | PropertyBindingInfo;
 
-		/**
-		 * The color to use (default to "Default" color).
-		 */
-		color?: ExampleColor | PropertyBindingInfo | `{${string}}`;
+    /**
+     * Interface defining the settings object used in constructor calls
+     */
+    interface $ExampleSettings extends $ControlSettings {
 
-		/**
-		 * Event is fired when the user clicks the control.
-		 */
-		press?: (event: Event) => void;
-	}
+        /**
+         * The text to display.
+         */
+        text?: string | PropertyBindingInfo;
 
-	export default interface Example {
-		// property: text
+        /**
+         * The color to use (default to "Default" color).
+         */
+        color?: ExampleColor | PropertyBindingInfo | `{${string}}`;
 
-		/**
-		 * Gets current value of property "text".
-		 *
-		 * The text to display.
-		 *
-		 * @returns Value of property "text"
-		 */
-		getText(): string;
+        /**
+         * Event is fired when the user clicks the control.
+         */
+        press?: (event: Example$PressEvent) => void;
+    }
 
-		/**
-		 * Sets a new value for property "text".
-		 *
-		 * The text to display.
-		 *
-		 * When called with a value of "null" or "undefined", the default value of the property will be restored.
-		 *
-		 * @param text New value for property "text"
-		 * @returns Reference to "this" in order to allow method chaining
-		 */
-		setText(text: string): this;
+    export default interface Example {
 
-		// property: color
+        // property: text
 
-		/**
-		 * Gets current value of property "color".
-		 *
-		 * The color to use (default to "Default" color).
-		 *
-		 * Default value is: "ExampleColor.Default,"
-		 * @returns Value of property "color"
-		 */
-		getColor(): ExampleColor;
+        /**
+         * The text to display.
+         */
+        getText(): string;
 
-		/**
-		 * Sets a new value for property "color".
-		 *
-		 * The color to use (default to "Default" color).
-		 *
-		 * When called with a value of "null" or "undefined", the default value of the property will be restored.
-		 *
-		 * Default value is: "ExampleColor.Default,"
-		 * @param [color="ExampleColor.Default,"] New value for property "color"
-		 * @returns Reference to "this" in order to allow method chaining
-		 */
-		setColor(color: ExampleColor): this;
+        /**
+         * The text to display.
+         */
+        setText(text: string): this;
 
-		// event: press
+        // property: color
 
-		/**
-		 * Attaches event handler "fn" to the "press" event of this "Example".
-		 *
-		 * Event is fired when the user clicks the control.
-		 *
-		 * When called, the context of the event handler (its "this") will be bound to "oListener" if specified,
-		 * otherwise it will be bound to this "Example" itself.
-		 *
-		 * @param fn The function to be called when the event occurs
-		 * @param listener Context object to call the event handler with. Defaults to this "Example" itself
-		 *
-		 * @returns Reference to "this" in order to allow method chaining
-		 */
-		attachPress(fn: (event: Event) => void, listener?: object): this;
+        /**
+         * The color to use (default to "Default" color).
+         */
+        getColor(): ExampleColor;
 
-		/**
-		 * Attaches event handler "fn" to the "press" event of this "Example".
-		 *
-		 * Event is fired when the user clicks the control.
-		 *
-		 * When called, the context of the event handler (its "this") will be bound to "oListener" if specified,
-		 * otherwise it will be bound to this "Example" itself.
-		 *
-		 * @param data An application-specific payload object that will be passed to the event handler along with the event object when firing the event
-		 * @param fn The function to be called when the event occurs
-		 * @param listener Context object to call the event handler with. Defaults to this "Example" itself
-		 *
-		 * @returns Reference to "this" in order to allow method chaining
-		 */
-		attachPress<CustomDataType extends object>(data: CustomDataType, fn: (event: Event, data: CustomDataType) => void, listener?: object): this;
+        /**
+         * The color to use (default to "Default" color).
+         */
+        setColor(color: ExampleColor): this;
 
-		/**
-		 * Detaches event handler "fn" from the "press" event of this "Example".
-		 *
-		 * Event is fired when the user clicks the control.
-		 *
-		 * The passed function and listener object must match the ones used for event registration.
-		 *
-		 * @param fn The function to be called, when the event occurs
-		 * @param listener Context object on which the given function had to be called
-		 * @returns Reference to "this" in order to allow method chaining
-		 */
-		detachPress(fn: (event: Event) => void, listener?: object): this;
+        // event: press
 
-		/**
-		 * Fires event "press" to attached listeners.
-		 *
-		 * Event is fired when the user clicks the control.
-		 *
-		 * @param parameters Parameters to pass along with the event
-		 * @returns Reference to "this" in order to allow method chaining
-		 */
-		firePress(parameters?: object): this;
-	}
+        /**
+         * Event is fired when the user clicks the control.
+         */
+        attachPress(fn: (event: Example$PressEvent) => void, listener?: object): this;
+
+        /**
+         * Event is fired when the user clicks the control.
+         */
+        attachPress<CustomDataType extends object>(data: CustomDataType, fn: (event: Example$PressEvent, data: CustomDataType) => void, listener?: object): this;
+
+        /**
+         * Event is fired when the user clicks the control.
+         */
+        detachPress(fn: (event: Example$PressEvent) => void, listener?: object): this;
+
+        /**
+         * Event is fired when the user clicks the control.
+         */
+        firePress(parameters?: Example$PressEventParameters): this;
+    }
+
+    /**
+     * Interface describing the parameters of Example's 'press' event.
+     * Event is fired when the user clicks the control.
+     */
+    // eslint-disable-next-line
+    export interface Example$PressEventParameters {
+    }
+
+    /**
+     * Type describing the Example's 'press' event.
+     * Event is fired when the user clicks the control.
+     */
+    export type Example$PressEvent = Event<Example$PressEventParameters>;
 }
