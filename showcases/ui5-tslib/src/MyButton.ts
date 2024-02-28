f95e67de2f50b3ad42939945afeb15c759b92777
Ben Reim <benj.reim@gmail.com>
2024-02-28 10:02:52 +0100

    feat(ui5-tslib): test scenario

---
+++ showcases/ui5-tslib/src/MyButton.ts
@@ -0,0 +1,10 @@
+import Button from "sap/m/Button";
+
+/**
+ * @name ui5.ecosystem.demo.tslib.MyButton
+ */
+export default class MyButton extends Button {
+	static renderer: {
+		apiVersion: 2;
+	};
+}
