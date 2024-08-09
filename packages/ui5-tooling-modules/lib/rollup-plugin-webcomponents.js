8e07385afdcf9cbd9ea9b08bc13271fc2af7cf37
Peter Muessig <peter.muessig@sap.com>
2024-08-09 16:45:19 +0200

    Resolve dependencies for metadata

--- packages/ui5-tooling-modules/lib/rollup-plugin-webcomponents.js
+++ packages/ui5-tooling-modules/lib/rollup-plugin-webcomponents.js
@@ -4,11 +4,12 @@ const WebComponentRegistry = require('./utils/WebComponentRegistry');
 
 const { compile } = require("handlebars");
 
-// Inspired by https://rollupjs.org/plugin-development/#resolveid (the Polyfill Injection)
-const SUFFIX_WEBC = "?webcomponent";
-const SUFFIX_WEBC_LIB = "?webcomponentlibrary";
-
 module.exports = function ({ resolveModule } = {}) {
+
+	const getNpmPackageName = (source) => {
+		const npmPackageScopeRegEx = /^((?:(@[^/]+)\/)?([^/]+))(?:\/(.*))?$/;
+		return npmPackageScopeRegEx.exec(source)?.[1];
+	};
 
 	const loadAndCompileTemplate = (templatePath) => {
 		const templateFile = readFileSync(join(__dirname, templatePath), { encoding: "utf-8" });
@@ -21,53 +22,66 @@ module.exports = function ({ resolveModule } = {}) {
 	const absToRelPathLib = {};
 	const absToRelPathWebC = {};
 
-	const lookupWebComponentsClass = (source) => {
-		const absModulePath = resolveModule(source);
-		if (WebComponentRegistry.getClassDefinition(absModulePath)) {
-			return WebComponentRegistry.getClassDefinition(absModulePath);
-		}
-
-		// determine npm package
-		const npmPackageScopeRegEx = /^((?:(@[^/]+)\/)?([^/]+))(?:\/(.*))?$/;
-		const npmPackage = npmPackageScopeRegEx.exec(source)?.[1];
-
+	const loadNpmPackage = (npmPackage) => {
 		let registryEntry = WebComponentRegistry.getPackage(npmPackage);
-
 		if (!registryEntry) {
 			const packageJsonPath = resolveModule(`${npmPackage}/package.json`);
 			if (packageJsonPath) {
 				const packageJson = require(packageJsonPath);
+				if (/^@ui5\/webcomponents/.test(packageJson.name)) {
+					packageJson.customElements = packageJson.customElements || "dist/custom-elements-internal.json";
+				}
 				if (!registryEntry && packageJson.customElements) {
+					Object.keys(packageJson.dependencies || {}).forEach((dep) => {
+						loadNpmPackage(dep);
+					});
+
 					// load custom elements metadata
 					// load custom elements metadata (custom-elements-internal.json > custom-elements.json)
-					let metadataPath = resolveModule(join(npmPackage, packageJson.customElements.replace("custom-elements.json", "custom-elements-internal.json")));
-					if (!metadataPath) {
-						metadataPath = resolveModule(join(npmPackage, packageJson.customElements));
-					}
-					const customElementsMetadata = require(metadataPath);
+					const metadataPath = resolveModule(join(npmPackage, packageJson.customElements));
+					if (metadataPath) {
+						const customElementsMetadata = require(metadataPath);
 
-					// first time registering a new Web Component package
-					const npmPackagePath = dirname(packageJsonPath);
-					registryEntry = WebComponentRegistry.register({
-						customElementsMetadata,
-						namespace: npmPackage,
-						npmPackagePath
-					});
-					absToRelPathLib[npmPackagePath] = npmPackage;
+						// first time registering a new Web Component package
+						const npmPackagePath = dirname(packageJsonPath);
+						registryEntry = WebComponentRegistry.register({
+							customElementsMetadata,
+							namespace: npmPackage,
+							npmPackagePath
+						});
+						absToRelPathLib[`${npmPackagePath}/library.js`] = npmPackage;
+					}
 				}
 			}
 		}
+		return registryEntry;
+	};
 
+	const lookupWebComponentsClass = (source) => {
+		let absModulePath = resolveModule(source);
+
+		let clazz;
+		if ((clazz = WebComponentRegistry.getClassDefinition(absModulePath))) {
+			return clazz;
+		}
+
+		// determine npm package
+		const npmPackage = getNpmPackageName(source);
+
+		const registryEntry = loadNpmPackage(npmPackage);
 		if (registryEntry) {
+			absModulePath = absModulePath || `${registryEntry.npmPackagePath}/library.js`;
 			const metadata = registryEntry;
 			const modulePath = absModulePath.substr(metadata.npmPackagePath.length + 1);
 			const moduleName = `${npmPackage}/${modulePath}`;
 
 			const clazz = WebComponentRegistry.getClassDefinition(moduleName);
-			clazz.modulePath = absModulePath;
-			clazz.moduleName = moduleName;
-			absToRelPathWebC[absModulePath] = moduleName;
-			return clazz;
+			if (clazz) {
+				clazz.modulePath = absModulePath;
+				clazz.moduleName = moduleName;
+				absToRelPathWebC[absModulePath] = moduleName;
+				return clazz;
+			}
 		}
 	}
 
@@ -85,74 +99,63 @@ module.exports = function ({ resolveModule } = {}) {
 					id: source,
 					external: true
 				};
-			} else if (/^(.*)\/library(?:.js)?$/.test(source)) {
-				// remove /library using regex
-				const parts = /^(.*)\/library(?:.js)?$/.exec(source);
-				const package = parts && WebComponentRegistry.getPackage(parts[1]);
-				if (package) {
-					return `${package.npmPackagePath}/library${SUFFIX_WEBC_LIB}`;
-				}
-			} else {
-				// find the Web Components class for the given module
-				const clazz = lookupWebComponentsClass(source);
-				if (clazz) {
-					return `${clazz.modulePath}${SUFFIX_WEBC}`;
-				}
+			}
+
+			const npmPackageScopeRegEx = /^((?:(@[^/]+)\/)?([^/]+))(?:\/(.*))?$/;
+			const npmPackage = npmPackageScopeRegEx.exec(source)?.[1];
+
+			let clazz, lib;
+			if ((clazz = lookupWebComponentsClass(source))) {
+				return `${clazz.modulePath}?control`;
+			} else if ((lib = WebComponentRegistry.getPackage(npmPackage))) {
+				return {
+					id: `${lib.npmPackagePath}/library.js`,
+					attributes: {
+						npmPackage
+					}
+				};
 			}
 		},
 
 		async load(id) {
 			// TODO:
 			// - @ui5/webcomponents/Button should re-export @ui5/webcomponents/dist/Button
-			if (id.indexOf(SUFFIX_WEBC_LIB) !== -1) {
-				const entryId = id.slice(0, id.indexOf(SUFFIX_WEBC_LIB));
-				const parts = /^(.*)\/library(?:.js)?$/.exec(entryId);
-				const packageName = parts && absToRelPathLib[parts[1]];
-				const package = WebComponentRegistry.getPackage(packageName);
-				if (package) {
-					const { namespace } = package;
+			let lib, clazz;
+			if ((lib = WebComponentRegistry.getPackage(absToRelPathLib[id]))) {
+				const { namespace } = lib;
 
-					// compile the library metadata
-					const metadataObject = {
-						apiVersion: 2,
-						name: namespace,
-						dependencies: ["sap.ui.core"],
-						types:	Object.keys(package.enums).map((enumName) => `${namespace}.${enumName}`),
-						elements: [ /* do we have any? */ ],
-						controls: Object.keys(package.customElements).map((elementName) => `${namespace}.${elementName}`),
-						interfaces: Object.keys(package.interfaces).map((interfaceName) => `${namespace}.${interfaceName}`),
-						designtime: `${namespace}/designtime/library.designtime`,
-						extensions: {
-							flChangeHandlers: {
-								"@ui5/webcomponents.Avatar": {
-									"hideControl": "default",
-									"unhideControl": "default"
-								},
-								"@ui5/webcomponents.Button": "@ui5/webcomponents-flexibility.Button",
-							}
-						},
-						noLibraryCSS: true,
-					};
-					const metadata = JSON.stringify(metadataObject, undefined, 2);
+				// compile the library metadata
+				const metadataObject = {
+					apiVersion: 2,
+					name: namespace,
+					dependencies: ["sap.ui.core"],
+					types:	Object.keys(lib.enums).map((enumName) => `${namespace}.${enumName}`),
+					elements: [ /* do we have any? */ ],
+					controls: Object.keys(lib.customElements).map((elementName) => `${namespace}.${elementName}`),
+					interfaces: Object.keys(lib.interfaces).map((interfaceName) => `${namespace}.${interfaceName}`),
+					designtime: `${namespace}/designtime/library.designtime`,
+					extensions: {
+						flChangeHandlers: {
+							"@ui5/webcomponents.Avatar": {
+								"hideControl": "default",
+								"unhideControl": "default"
+							},
+							"@ui5/webcomponents.Button": "@ui5/webcomponents-flexibility.Button",
+						}
+					},
+					noLibraryCSS: true,
+				};
+				const metadata = JSON.stringify(metadataObject, undefined, 2);
 
-					// generate the library code
-					const code = libTemplateFn({
-						metadata,
-						namespace,
-						enums: package.enums,
-					});
-					return code;
-				}
+				// generate the library code
+				const code = libTemplateFn({
+					metadata,
+					namespace,
+					enums: lib.enums,
+				});
+				return code;
 
-			} else if (id.indexOf(SUFFIX_WEBC) !== -1) {
-				// get the WebComponent class definition
-				const entryId = id.slice(0, id.indexOf(SUFFIX_WEBC));
-				const moduleName = absToRelPathWebC[entryId];
-				const clazz = WebComponentRegistry.getClassDefinition(moduleName);
-				if (!clazz) {
-					return null;
-				}
-
+			} else if (id.endsWith("?control") && (clazz = WebComponentRegistry.getClassDefinition(absToRelPathWebC[id.split("?")[0]]))) {
 				// Extend the superclass with the WebComponent class and export it
 				const ui5Metadata = clazz._ui5metadata;
 				const ui5Class = `${ui5Metadata.namespace}.${clazz.name}`;
--- showcases/ui5-tsapp-simple/webapp/controller/Main.controller.ts
+++ showcases/ui5-tsapp-simple/webapp/controller/Main.controller.ts
@@ -1,3 +1,6 @@
+import hyphenate from "sap/base/strings/hyphenate";
+import WebComponent from "sap/ui/core/webc/WebComponent";
+import WebComponentRenderer from "sap/ui/core/webc/WebComponentRenderer";
 import Controller from "sap/ui/core/mvc/Controller";
 import MessageToast from "sap/m/MessageToast";
 import Button from "@ui5/webcomponents/Button";
@@ -6,6 +9,41 @@ import Control from "sap/ui/core/Control";
 import VBox from "sap/m/VBox";
 import Popup from "sap/ui/core/Popup";
 import Event from "sap/ui/base/Event";
+//import "@ui5/webcomponents/library";
+
+WebComponentRenderer.renderAttributeProperties = function(oRm, oWebComponent) {
+	var oAttrProperties = oWebComponent.getMetadata().getPropertiesByMapping("property");
+	var aPropsToAlwaysSet = ["enabled"].concat(Object.entries(oWebComponent.getMetadata().getPropertyDefaults()).map(([key, value]) => {
+		return value !== undefined && value !== false && ? key : null;
+	})); // some properties can be initial and still have a non-default value due to side effects (e.g. EnabledPropagator)
+	for (var sPropName in oAttrProperties) {
+		if (oWebComponent.isPropertyInitial(sPropName) && !aPropsToAlwaysSet.includes(sPropName)) {
+			continue; // do not set attributes for properties that were not explicitly set or bound
+		}
+
+		var oPropData = oAttrProperties[sPropName];
+		var vPropValue = oPropData.get(oWebComponent);
+		if (oPropData.type === "object" || typeof vPropValue === "object") {
+			continue; // Properties of type "object" and custom-type properties with object values are set during onAfterRendering
+		}
+
+		var sAttrName = oPropData._sMapTo ? oPropData._sMapTo : hyphenate(sPropName);
+		if (oPropData._fnMappingFormatter) {
+			vPropValue = oWebComponent[oPropData._fnMappingFormatter].call(oWebComponent, vPropValue);
+		}
+
+		if (oPropData.type === "boolean") {
+			if (vPropValue) {
+				oRm.attr(sAttrName, "");
+			}
+		} else {
+			if (vPropValue != null) {
+				oRm.attr(sAttrName, vPropValue);
+			}
+		}
+	}
+};
+
 
 /*
 
