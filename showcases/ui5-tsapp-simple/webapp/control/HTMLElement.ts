31ad3a261fce438bfcaaef3f81bf0364cfc7018d
Thorsten Hochreuter <thorsten.hochreuter@sap.com>
2024-08-16 12:06:02 +0200

    chore: finish fiori.DynamicPage example

---
+++ showcases/ui5-tsapp-simple/webapp/control/HTMLElement.ts
@@ -0,0 +1,50 @@
+import Control from "sap/ui/core/Control";
+import RenderManager from "sap/ui/core/RenderManager";
+import type { MetadataOptions } from "sap/ui/core/Element";
+
+/**
+ * @namespace ui5.ecosystem.demo.simpletsapp.control
+ */
+export default class HTMLElement extends Control {
+	static readonly metadata: MetadataOptions = {
+		properties: {
+			text: "string",
+			tag: "string",
+		},
+		aggregations: {
+			children: { type: "sap.ui.core.Control", multiple: true },
+		},
+		defaultAggregation: "children",
+	};
+
+	// The following three lines were generated and should remain as-is to make TypeScript aware of the constructor signatures
+	constructor(idOrSettings?: string | $HTMLElementSettings);
+	constructor(id?: string, settings?: $HTMLElementSettings);
+	constructor(id?: string, settings?: $HTMLElementSettings) {
+		super(id, settings);
+	}
+
+	renderer = {
+		apiVersion: 2,
+		render: (rm: RenderManager, control: HTMLElement) => {
+			rm.openStart(control.getTag(), control);
+
+			Object.keys(control.getMetadata().getProperties()).forEach((name) => {
+				if (name != "tag") {
+					const value = control.getProperty(name) as string;
+					if (value) {
+						rm.attr(name, value);
+					}
+				}
+			});
+
+			rm.openEnd();
+
+			control.getChildren().forEach((child) => {
+				rm.renderControl(child);
+			});
+
+			rm.close("div");
+		},
+	};
+}
--- showcases/ui5-tsapp-simple/webapp/controller/DynamicPage.controller.ts
+++ showcases/ui5-tsapp-simple/webapp/controller/DynamicPage.controller.ts
@@ -1,14 +1,112 @@
 /* eslint-disable */
 import Controller from "sap/ui/core/mvc/Controller";
-import Control from "sap/ui/core/Control";
-import VBox from "sap/m/VBox";
-import Popup from "sap/ui/core/Popup";
-import Event from "sap/ui/base/Event";
-//import "@ui5/webcomponents/library";
+import JSONModel from "sap/ui/model/json/JSONModel";
+
+function injectStyle() {
+	const sheet = new CSSStyleSheet();
+	sheet.replaceSync(`
+.text {
+	display: inline-block;
+	font-size: var(--sapFontSize);
+	font-family: var(--sapFontFamily);
+	color: var(--sapTextColor);
+	line-height: normal;
+	white-space: pre-line;
+	word-wrap: break-word;
+	cursor: text;
+	margin: 0;
+}
+
+.product-info {
+	display: flex;
+	flex-wrap: wrap;
+}
+
+.product-info [ui5-avatar],
+.product-info .product-info-cell {
+	margin-right: 2rem;
+	margin-bottom: 1rem;
+}
+
+.product-info-cell {
+	display: flex;
+	gap: 5px;
+	flex-direction: column;
+}
+
+.product-description {
+	display: inline;
+	max-width: 300px;
+}
+
+.availability {
+	font-size: 1.2rem;
+	color: var(--sapPositiveTextColor);
+}
+
+.price {
+	font-size: 1.2rem;
+	color: var(--sapTextColor);
+}
+
+.snapped-title-heading {
+	display: flex;
+	align-items: center;
+}
+.snapped-title-heading [ui5-avatar] {
+	margin-right: 1rem;
+}
+.snapped-title-heading [ui5-title] {
+	font-family: var(--sapObjectHeader_Title_FontFamily);
+	color: var(--sapObjectHeader_Title_TextColor);
+	padding: 0.3125rem 0 0 0;
+	font-size: var(--sapObjectHeader_Title_SnappedFontSize);
+	text-overflow: ellipsis;
+	min-width: 0;
+}
+`);
+	document.adoptedStyleSheets.push(sheet);
+}
 
 /**
  * @namespace ui5.ecosystem.demo.simpletsapp.controller
  */
 export default class DynamicPage extends Controller {
-	public onInit(): void {}
+	public onInit(): void {
+		injectStyle();
+
+		// generate some sample data
+		const sampleDescriptions = [
+			"10 inch Portable DVD",
+			"7 inch WidescreenPortable DVD Player w MP3",
+			"Astro Laptop 1516",
+			"Astro Phone 6",
+			"Audio/Video Cable Kit - 4m",
+			"Beam Breaker B-1",
+			"Beam Breaker B-2",
+			"Beam Breaker B-3",
+			"Beam Breaker B-4",
+			"Camcorder View",
+			"Benda Laptop 1408",
+			"Cepat Tablet 10.5",
+		];
+
+		const images = ["HT-1000.jpg", "HT-1010.jpg", "HT-1022.jpg", "HT-1030.jpg", "HT-2002.jpg", "HT-2026.jpg"];
+
+		const items = [];
+		for (let i = 0; i < 20; i++) {
+			items.push({
+				productID: `HT-${i}`,
+				productName: sampleDescriptions[(Math.random() * sampleDescriptions.length) | 0],
+				price: `${i.toFixed(2)} EUR`,
+				imageSrc: `https://sap.github.io/ui5-webcomponents/images/${images[i % images.length]}`,
+			});
+		}
+
+		const model = new JSONModel({
+			listTitle: "Products",
+			items,
+		});
+		this.getView().setModel(model, "dpModel");
+	}
 }
