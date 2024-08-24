59206b67050826642dbc109f118bebb9e3b695cf
Peter Muessig <peter.muessig@sap.com>
2024-08-24 20:40:10 +0200

    chore: next try to fix the wdi5 issues

--- showcases/ui5-app/webapp/test/e2e/binding.test.js
+++ showcases/ui5-app/webapp/test/e2e/binding.test.js
@@ -1,13 +1,6 @@
 const MainPage = require("./pages/Main");
 const onTheOtherPage = require("./pages/Other");
 
-const list = {
-	selector: {
-		viewName: "ui5.ecosystem.demo.app.view.Other",
-		id: "PeopleList",
-	},
-};
-
 // this suite implemented straigh-fwd, no page objects
 describe("binding", function () {
 	it("should see the initial page of the app", async function () {
@@ -17,33 +10,6 @@ describe("binding", function () {
 
 	it("Other view: PeopleList: items aggregation + amount of items", async function () {
 		await MainPage.iPressTheNavButton();
-
-		expect(onTheOtherPage.iShouldSeeTheList()).toBeTruthy();
-
-		/*
-		console.log("list", await browser
-			.asControl({
-				selector: {
-					viewName: "ui5.ecosystem.demo.app.view.Other",
-					id: "PeopleList",
-				},
-			}));
-
-		const isVisible = (await browser
-			.asControl({
-				selector: {
-					viewName: "ui5.ecosystem.demo.app.view.Other",
-					id: "PeopleList",
-				},
-			})
-			.getVisible()) === true;
-		expect(isVisible).toBeTruthy();
-
-		console.log("list2", await browser.asControl(list));
-		*/
-
-		// ui5 api + high-speed aggregation retrieval: https://ui5-community.github.io/wdi5/#/usage?id=getshorthand-conveniences
-		//const aListItems = await browser.asControl(list).getItems(true);
-		//expect(aListItems.length).toBeGreaterThanOrEqual(1);
+		expect(onTheOtherPage.iShouldSeeListItems()).toBeTruthy();
 	});
 });
--- showcases/ui5-app/webapp/test/e2e/pages/Other.js
+++ showcases/ui5-app/webapp/test/e2e/pages/Other.js
@@ -3,17 +3,20 @@ const Page = require("./Page");
 class Other extends Page {
 	_viewName = "ui5.ecosystem.demo.app.view.Other";
 
+	_listPeople = {
+		selector: {
+			viewName: "ui5.ecosystem.demo.app.view.Other",
+			id: "PeopleList",
+		},
+	};
+
 	async iShouldSeeTheList() {
-		return (
-			(await browser
-				.asControl({
-					selector: {
-						viewName: "ui5.ecosystem.demo.app.view.Other",
-						id: "PeopleList",
-					},
-				})
-				.getVisible()) === true
-		);
+		return (await browser.asControl(this._listPeople).getVisible()) === true;
+	}
+
+	async iShouldSeeListItems() {
+		// ui5 api + high-speed aggregation retrieval: https://ui5-community.github.io/wdi5/#/usage?id=getshorthand-conveniences
+		return (await browser.asControl(this._listPeople).getItems()).length > 0;
 	}
 
 	async iNavigateBack() {
