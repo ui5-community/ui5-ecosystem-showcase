e07c1cf38029b54bb2ce9eaa62e0d84d0ae1c38d
Peter Muessig <peter.muessig@sap.com>
2024-08-24 20:55:14 +0200

    chore: next try to fix the wdi5 issues

--- showcases/ui5-app/webapp/test/e2e/binding.test.js
+++ showcases/ui5-app/webapp/test/e2e/binding.test.js
@@ -1,6 +1,14 @@
 const MainPage = require("./pages/Main");
 const onTheOtherPage = require("./pages/Other");
 
+const list = {
+	selector: {
+		viewName: "ui5.ecosystem.demo.app.view.Other",
+		id: "PeopleList",
+	},
+};
+
+// this suite implemented straigh-fwd, no page objects
 describe("binding", function () {
 	it("should see the initial page of the app", async function () {
 		await MainPage.open();
@@ -11,5 +19,9 @@ describe("binding", function () {
 		await MainPage.iPressTheNavButton();
 		expect(onTheOtherPage.iShouldSeeTheList()).toBeTruthy();
 		expect(onTheOtherPage.iShouldSeeListItems()).toBeTruthy();
+
+		// ui5 api + high-speed aggregation retrieval: https://ui5-community.github.io/wdi5/#/usage?id=getshorthand-conveniences
+		const aListItems = await browser.asControl(list).getItems(true);
+		expect(aListItems.length).toBeGreaterThanOrEqual(1);
 	});
 });
