cf99a683d33102b6df57a439afb5c96913921616
Peter Muessig <peter.muessig@sap.com>
2025-04-14 13:31:37 +0200

    fix(ui5-tooling-module): ensure exports for mods using exports.default

---
+++ packages/ui5-tooling-modules/test/__snap__/64fb0379/@ui5/webcomponents/dist/Button.js
@@ -0,0 +1,123 @@
+/*!
+ * ${copyright}
+ */
+sap.ui.define([
+  "sap/ui/core/webc/WebComponent",
+  "sap/ui/core/EnabledPropagator",
+  "ui5/ecosystem/demo/app/resources/@ui5/webcomponents",
+  "ui5/ecosystem/demo/app/resources/Button",
+], function(
+  WebComponentBaseClass,
+  EnabledPropagator,
+) {
+  "use strict";
+
+  const WrapperClass = WebComponentBaseClass.extend("@ui5/webcomponents.Button", {
+    metadata:
+{
+  "namespace": "@ui5/webcomponents",
+  "tag": "ui5-button",
+  "interfaces": [
+    "@ui5/webcomponents.IButton"
+  ],
+  "properties": {
+    "design": {
+      "type": "@ui5/webcomponents.ButtonDesign",
+      "mapping": "property",
+      "defaultValue": "Default"
+    },
+    "enabled": {
+      "type": "boolean",
+      "defaultValue": "true",
+      "mapping": {
+        "type": "property",
+        "to": "disabled",
+        "formatter": "_mapEnabled"
+      }
+    },
+    "icon": {
+      "type": "string",
+      "mapping": "property"
+    },
+    "endIcon": {
+      "type": "string",
+      "mapping": "property"
+    },
+    "submits": {
+      "type": "boolean",
+      "mapping": "property",
+      "defaultValue": false
+    },
+    "tooltip": {
+      "type": "string",
+      "mapping": "property"
+    },
+    "accessibleName": {
+      "type": "string",
+      "mapping": "property"
+    },
+    "accessibilityAttributes": {
+      "type": "object",
+      "mapping": "property",
+      "defaultValue": {}
+    },
+    "accessibleDescription": {
+      "type": "string",
+      "mapping": "property"
+    },
+    "type": {
+      "type": "@ui5/webcomponents.ButtonType",
+      "mapping": "property",
+      "defaultValue": "Button"
+    },
+    "accessibleRole": {
+      "type": "@ui5/webcomponents.ButtonAccessibleRole",
+      "mapping": "property",
+      "defaultValue": "Button"
+    },
+    "text": {
+      "type": "string",
+      "mapping": "textContent"
+    },
+    "width": {
+      "type": "sap.ui.core.CSSSize",
+      "mapping": "style"
+    },
+    "height": {
+      "type": "sap.ui.core.CSSSize",
+      "mapping": "style"
+    }
+  },
+  "aggregations": {
+    "badge": {
+      "type": "@ui5/webcomponents.ButtonBadge",
+      "multiple": true,
+      "slot": "badge"
+    }
+  },
+  "associations": {
+    "ariaLabelledBy": {
+      "type": "sap.ui.core.Control",
+      "multiple": true,
+      "mapping": {
+        "type": "property",
+        "to": "accessibleNameRef",
+        "formatter": "_getAriaLabelledByForRendering"
+      }
+    }
+  },
+  "events": {
+    "click": {}
+  },
+  "getters": [],
+  "methods": [],
+  "library": "@ui5/webcomponents.library",
+  "designtime": "@ui5/webcomponents/designtime/Button.designtime"
+}
+  });
+
+  EnabledPropagator.call(WrapperClass.prototype);
+
+  return WrapperClass;
+
+});
