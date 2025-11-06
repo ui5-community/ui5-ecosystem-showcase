0829d09b8eda85e7b0acf735f0203fae4c1a7112
Peter Muessig <peter.muessig@sap.com>
2025-11-06 20:55:59 +0100

    fix(ui5-tooling-modules): force include assets

--- showcases/ui5-tsapp-webc/webapp/Component.ts
+++ showcases/ui5-tsapp-webc/webapp/Component.ts
@@ -1,8 +1,9 @@
 import UIComponent from "sap/ui/core/UIComponent";
 import { createDeviceModel } from "./model/models";
 
-import "@ui5/webcomponents/dist/Assets";
-import "@ui5/webcomponents-fiori/dist/Assets";
+// commented in favor of automatic inclusion via ui5.yaml
+//import "@ui5/webcomponents/dist/Assets";
+//import "@ui5/webcomponents-fiori/dist/Assets";
 
 /**
  * @namespace ui5.ecosystem.demo.webctsapp
